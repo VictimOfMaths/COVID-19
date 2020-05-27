b58c5835641f5641c9be022f7a6bf1be531a6b5e
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-05-27 10:16:41 +0100

    Added ridgeplot and fixed minor bar error

--- Heatmaps/Welsh LA Heatmaps.R
+++ Heatmaps/Welsh LA Heatmaps.R
@@ -19,7 +19,8 @@ data$date <- as.Date(data$date)
 heatmap <- data %>%
   group_by(LA) %>%
   mutate(casesroll_avg=roll_mean(cases, 5, align="left", fill=0)) %>%
-  mutate(maxcaserate=max(casesroll_avg), maxcaseday=date[which(casesroll_avg==maxcaserate)][1])
+  mutate(maxcaserate=max(casesroll_avg), maxcaseday=date[which(casesroll_avg==maxcaserate)][1],
+         allcases=max(totalcases))
 
 heatmap$maxcaseprop <- heatmap$casesroll_avg/heatmap$maxcaserate
 
@@ -40,14 +41,27 @@ casetiles <- ggplot(heatmap, aes(x=date, y=fct_reorder(LA, maxcaseday), fill=max
   theme(axis.line.y=element_blank(), plot.subtitle=element_text(size=rel(0.78)), plot.title.position="plot",
         axis.text.y=element_text(colour="Black"))
 
-casebars <- ggplot(subset(heatmap, date==maxcaseday), aes(x=totalcases, y=fct_reorder(LA, maxcaseday), fill=totalcases))+
+casebars <- ggplot(subset(heatmap, date==maxcaseday), aes(x=allcases, y=fct_reorder(LA, maxcaseday), fill=allcases))+
   geom_col(show.legend=FALSE)+
   theme_classic()+
   scale_fill_distiller(palette="Spectral")+
-  scale_x_continuous(name="Total confirmed cases", breaks=c(0,500,1000))+
+  scale_x_continuous(name="Total confirmed cases", breaks=c(0,500,1000,1500, 2000))+
   theme(axis.title.y=element_blank(), axis.line.y=element_blank(), axis.text.y=element_blank(),
         axis.ticks.y=element_blank(), axis.text.x=element_text(colour="Black"))
 
 tiff("Outputs/COVIDWelshLACasesHeatmap.tiff", units="in", width=10, height=6, res=500)
 plot_grid(casetiles, casebars, align="h", rel_widths=c(1,0.2))
+dev.off()
+
+library(ggridges)
+
+tiff("Outputs/COVIDWelshLACaseRidges.tiff", units="in", width=10, height=6, res=500)
+ggplot(heatmap, aes(x=date, y=fct_reorder(LA, allcases), height=cases, fill=cases))+
+  geom_density_ridges_gradient(stat="identity", rel_min_height=0.01)+
+  theme_classic()+
+  scale_fill_distiller(palette="Spectral", name="Cases per day\n5-day rolling avg.")+
+  scale_x_date(name="Date", limits=as.Date(c(plotfrom, plotto)), expand=c(0,0))+
+  scale_y_discrete(name="")+
+  labs(title="Timelines of confirmed COVID-19 cases in Welsh Local Authorities",
+       caption="Data from Public Health Wales | Plot by @VictimOfMaths")
 dev.off()
