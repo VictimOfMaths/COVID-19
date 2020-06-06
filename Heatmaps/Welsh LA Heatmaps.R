5ad7cd7c31412740c80130ad132b09721e9da281
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-06-06 20:18:19 +0100

    Update Welsh LA Heatmaps.R

--- Heatmaps/Welsh LA Heatmaps.R
+++ Heatmaps/Welsh LA Heatmaps.R
@@ -11,7 +11,7 @@ library(cowplot)
 temp <- tempfile()
 source <- "http://www2.nphs.wales.nhs.uk:8080/CommunitySurveillanceDocs.nsf/3dc04669c9e1eaa880257062003b246b/77fdb9a33544aee88025855100300cab/$FILE/Rapid%20COVID-19%20surveillance%20data.xlsx"
 temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
-data <- read_excel(temp, sheet=2)
+data <- read_excel(temp, sheet=3)
 
 colnames(data) <- c("LA", "date", "cases", "totalcases", "totalcaserate", "tests", "totaltests")
 data$date <- as.Date(data$date)
