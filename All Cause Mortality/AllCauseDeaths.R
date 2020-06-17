e646576b798ad65dbf6a713e90e4af8613765681
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-06-17 14:05:19 +0100

    New Scottish data

--- All Cause Mortality/AllCauseDeaths.R
+++ All Cause Mortality/AllCauseDeaths.R
@@ -14,17 +14,17 @@ library(ggtext)
 
 #Latest date in the country-specific data
 EWDate <- "5th June"
-ScotDate <- "6th June"
+ScotDate <- "13th June"
 NIDate <- "5th June"
 
 #Locations for latest data. Links for historical data don't move, so keep them further down
 Eng2020 <- "https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fbirthsdeathsandmarriages%2fdeaths%2fdatasets%2fweeklyprovisionalfiguresondeathsregisteredinenglandandwales%2f2020/publishedweek232020.xlsx"
-Scot2020 <- "https://www.nrscotland.gov.uk/files//statistics/covid19/covid-deaths-data-week-23.xlsx"
+Scot2020 <- "https://www.nrscotland.gov.uk/files//statistics/covid19/covid-deaths-data-week-24.xlsx"
 NI2020 <- "https://www.nisra.gov.uk/sites/nisra.gov.uk/files/publications/Weekly_Deaths.xls"
 
 #Stupid Excel range controls
 EngRange <- "Y" #increment by one letter each week
-ScotRange <- "Y" #incrememnt by one letter each week
+ScotRange <- "Z" #incrememnt by one letter each week
 NIRange <- "27" #incremement by one number each week
 
 #Also need to manually add the next row of data for the deaths by location at the end.
@@ -994,7 +994,7 @@ S.excess <- data.S.new %>%
   summarise(excess=sum(excess), total=sum(mean), percexcess=excess/total)
 
 #Extract y=axis placement for excess deaths figure
-labpos <- 1300
+labpos <- 1700
 
 
 tiff("Outputs/NRSWeeklyDeaths.tiff", units="in", width=10, height=8, res=300)
@@ -1007,10 +1007,10 @@ ggplot()+
   scale_x_continuous(name="Week number", breaks=c(0,10,20,30,40,50))+
   scale_y_continuous(name="Deaths registered")+
   expand_limits(y=0)+
-  labs(title="All-cause deaths in Scotland continue to fall",
+  labs(title="All-cause deaths in Scotland are back at 'normal' levels",
        subtitle=paste0("Weekly deaths in <span style='color:red;'>2020</span> compared to <span style='color:Skyblue4;'>the range in 2010-19</span>. Data up to ", ScotDate, "."),
        caption="Data from NRS | Plot by @VictimOfMaths")+
-  annotate(geom="text", x=Scotmaxweek-1.5, y=labpos, label=paste0(round(S.excess$excess, 0), 
+  annotate(geom="text", x=Scotmaxweek-5.5, y=labpos, label=paste0(round(S.excess$excess, 0), 
                                                                   " more deaths in 2020 than average (+", round(S.excess$percexcess*100, 0),"%)"), colour="Red", hjust=0)+
   annotate(geom="text", x=30, y=1150, label="Historic maximum", colour="Skyblue4")+
   annotate(geom="text", x=30, y=800, label="Historic minimum", colour="Skyblue4")+
