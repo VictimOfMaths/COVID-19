4db779f803ed851ddfb804760c7a3729e25e9cb2
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-06-19 11:57:38 +0100

    Update COVID-19 vs. Brexit.R

--- Observed Inequality/COVID-19 vs. Brexit.R
+++ Observed Inequality/COVID-19 vs. Brexit.R
@@ -10,7 +10,8 @@ library(readxl)
 temp <- tempfile()
 source <- "http://researchbriefings.files.parliament.uk/documents/CBP-7639/EU-referendum-results-and-characteristics-data.xlsx"
 temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
-Brexitdata <- read_excel(temp, sheet="Results", range="A1:U383")[,c(4,5,20)]
+Brexitdata <- read_excel(temp, sheet="Results", range="A1:U383")[,c(4,5,19,20)]
+Brexitdata$Result <- if_else(Brexitdata$Pct_Leave>Brexitdata$Pct_Remain, "Leave", "Remain")
 
 #Read in COVID19 data
 #Deaths
@@ -35,5 +36,32 @@ ggplot(data, aes(x=Pct_Leave, y=as.numeric(Mortrate)))+
   theme_classic()+
   labs(title="Correlation between Brexit vote and COVID-19 mortality",
        subtitle="Based on Local Authorities in England & Wales",
+       caption="Data from House of Commons Library & ONS | Plot by @VictimOfMaths")
+dev.off()
+
+#Bring in unadjusted case trajectories
+temp <- tempfile()
+source <- "https://coronavirus.data.gov.uk/downloads/csv/coronavirus-cases_latest.csv"
+temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
+Casesdata <- read.csv(temp)[,c(2,1,4,5)]
+colnames(Casesdata) <- c("Area_Code", "Area", "Date", "NewCases")
+
+Casesdata$Date <- as.Date(Casesdata$Date)
+
+#Merge in Brexit vote
+Casesdata <- merge(Casesdata, Brexitdata, by="Area_Code")
+
+#Calculate trajectories
+casetraj <- Casesdata %>%
+  group_by(Date, Result) %>%
+  summarise(cases=sum(NewCases))
+
+tiff("Outputs/COVIDvsBrexitTrajectories.tiff", units="in", width=8, height=6, res=500)
+ggplot(casetraj, aes(x=Date, y=cases, colour=Result))+
+  geom_line()+
+  theme_classic()+
+  scale_colour_manual(values=c("#003399", "#FFCC00"))+
+  scale_y_continuous(name="Daily confirmed COVID-19 cases")+
+  labs(title="New COVID-19 cases by Local Authority Brexit vote",
        caption="Data from House of Commons Library & ONS | Plot by @VictimOfMaths")
 dev.off()
