dbf0cf986c051e1735ac53ef766a329abbb2ac95
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-07-06 20:23:55 +0100

    Added Scottish data

--- All Cause Mortality/LA All Cause Deaths.R
+++ All Cause Mortality/LA All Cause Deaths.R
@@ -20,7 +20,7 @@ data20$deaths <- as.numeric(data20$deaths)
 data20$week <- as.numeric(data20$week)
 
 maxweek <- max(data20$week)
-enddate <- as.Date("2020-01-03")+weeks(maxweek-1)
+enddate.ew <- as.Date("2020-01-03")+weeks(maxweek-1)
 
 #Read in 2015-19 historic data for England & Wales
 temp <- tempfile()
@@ -84,7 +84,96 @@ data20_wide <- merge(data20_wide, data1519, by=c("code", "name", "week"))[,-c(4,
 data20_wide$Other <- data20_wide$`All causes`-data20_wide$deaths
 data_bar <- gather(data20_wide, excesscause, deaths, c(5,7))
 
+#All-cause only dataset
+data.all <- merge(data1519, data20.all, by=c("code", "week", "name"), all.x=TRUE)[,c(1:4,7)]
+colnames(data.all) <- c("code", "week", "name", "hist", "2020")
+data.all <- data.all %>% drop_na(name)
+data.all$excess <- data.all$`2020`-data.all$hist
+
+#Bring in Scottish deaths data (released by NRS on a Wednesday)
+#2020 data
+temp <- tempfile()
+source <- "https://www.nrscotland.gov.uk/files//statistics/covid19/weekly-deaths-by-date-council-area-location.xlsx"
+temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
+data20.s <- read_excel(temp, sheet=2, range="A5:E3276", col_names=FALSE)
+colnames(data20.s) <- c("week", "name", "location", "cause", "deaths")
+data20.s$week <- as.numeric(data20.s$week)
+
+data20.s$location <- case_when(
+  data20.s$location=="Care Home" ~ "Care home",
+  data20.s$location %in% c("Home / Non-institution", "Other institution") ~ "Home/Other",
+  TRUE ~ "Hospital"
+)
+
+data20.s <- data20.s %>% 
+  group_by(week, name, location, cause) %>% 
+  summarise(deaths=sum(deaths))
+
+data20.s <- spread(data20.s, cause, deaths)
+data20.s$`COVID-19` <- if_else(is.na(data20.s$`COVID-19`),0,data20.s$`COVID-19`)
+data20.s$`Non-COVID-19` <- if_else(is.na(data20.s$`Non-COVID-19`),0,data20.s$`Non-COVID-19`)
+data20.s$All <- data20.s$`COVID-19`+data20.s$`Non-COVID-19`
+
+maxweek.s <- max(data20.s$week)
+enddate.s <- as.Date("2020-01-04")+weeks(maxweek.s-1)
+
+#2015-19 data
+temp <- tempfile()
+source <- "https://www.nrscotland.gov.uk/files//statistics/covid19/weekly-deaths-by-date-council-area-location-15-19.xlsx"
+temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
+data1519.s <- read_excel(temp, sheet=2, range="A5:E25207", col_names=FALSE)
+colnames(data1519.s) <- c("week", "name", "location", "year", "deaths")
+data1519.s$week <- as.numeric(data1519.s$week)
+
+data1519.s$location <- case_when(
+  data1519.s$location=="Care Home" ~ "Care home",
+  data1519.s$location %in% c("Home / Non-institution", "Other institution") ~ "Home/Other",
+  TRUE ~ "Hospital"
+)
+
+data1519.s <- data1519.s %>% 
+  group_by(week, name, location) %>% 
+  summarise(hist=mean(deaths))
+
+#Faff about aligning Scottish data with English/Welsh data 
+#(a clever person would have a much more elegant solution to all this, I'm sure)
+
+data.s <- merge(data1519.s, data20.s, by=c("week", "name", "location"),all=TRUE)
+data.s$`COVID-19` <- if_else(is.na(data.s$`COVID-19`) & data.s$week<=maxweek.s, 0, data.s$`COVID-19`)
+data.s$`Non-COVID-19` <- if_else(is.na(data.s$`Non-COVID-19`) & data.s$week<=maxweek.s, 0, data.s$`Non-COVID-19`)
+data.s$All <- if_else(is.na(data.s$All) & data.s$week<=maxweek.s, 0, data.s$All)
+
+data.all.s <- data.s %>% 
+  select(c(1:4,7)) %>% 
+  group_by(week, name) %>% 
+  summarise(hist=sum(hist), `2020`=sum(All)) %>% 
+  mutate(excess=`2020`-hist)
+
+data.all <- bind_rows(data.all, data.all.s)
+
+data.s$otherexcess <- data.s$`Non-COVID-19`-data.s$hist
+
+data_bar.s <- data.s %>% 
+  group_by(name, week) %>% 
+  summarise(`COVID 19`=sum(`COVID-19`), Other=sum(otherexcess))
+
+data_bar.s <- gather(data_bar.s, excesscause, deaths, c(3,4))
+
+data_bar <- bind_rows(data_bar, data_bar.s)
+
+data.loc.s <- data.s[,c(1:4, 7)]
+colnames(data.loc.s) <- c("week", "name", "location", "deaths1519", "deaths20")
+data.loc.s$excess <- data.loc.s$deaths20-data.loc.s$deaths1519
+
+data.loc <- bind_rows(data.loc, data.loc.s)
+
 #Summary figures
+data_summary.s <- data.s %>% 
+  filter(week<=maxweek.s) %>% 
+  group_by(name) %>% 
+  summarise(hist=sum(hist), curr=sum(All)) %>% 
+  mutate(excess=curr-hist, excessprop=excess/hist, country="Scotland")
+
 data_summary <- data20_wide %>% 
   group_by(code, name) %>% 
   summarise(hist=sum(deaths), curr=sum(`All causes`)) %>% 
@@ -92,12 +181,17 @@ data_summary <- data20_wide %>%
 
 data_summary$country <- if_else(substr(data_summary$code,1,1)=="E", "England", "Wales")
 
+data_summary <- bind_rows(data_summary, data_summary.s)
+
+tiff("Outputs/ExcessLAGB.tiff", units="in", width=8, height=32, res=500)
 ggplot(data_summary, aes(x=excessprop*100, y=fct_reorder(name, excessprop), fill=country))+
   geom_col()+
   scale_x_continuous(name="Change in all-cause deaths in 2020 vs. 2015-19 mean", 
                      breaks=c(-25,0,25,50), labels=c("-25%", "0%", "+25%", "+50%"))+
   scale_y_discrete(name="Local Authority")+
+  scale_fill_manual(values=c("#B01824", "#82D0F0", "#326529"), name="Country")+
   theme_classic()
+dev.off()
 
 #Read in cases data for England
 temp <- tempfile()
@@ -174,15 +268,12 @@ pillardata <- pillardata %>%
 
 pillardata_long <- gather(pillardata, pillar, cases, c(8,9))
 
-#All-cause only dataset
-data.all <- merge(data1519, data20.all, by=c("code", "week", "name"), all.x=TRUE)[,c(1:4,7)]
-colnames(data.all) <- c("code", "week", "name", "hist", "2020")
-data.all <- data.all %>% drop_na(name)
-data.all$excess <- data.all$`2020`-data.all$hist
-
 #LA level excess deaths
-LA <- "Sheffield"
+LA <- "Glasgow City"
 excess <- data_summary %>% filter(name==LA) 
+
+enddate <- if_else(excess[1,7]=="Scotland", enddate.s, enddate.ew)
+source <- if_else(excess[1,7]=="Scotland", "NRS", "ONS")
 
 labpos <- max(data.all$`2020`[data.all$week==maxweek & data.all$name==LA],
               data.all$hist[data.all$week==maxweek & data.all$name==LA])
@@ -204,7 +295,7 @@ ggplot(subset(data.all, name==LA))+
            hjust=0, colour="red", size=3)+
   labs(title=paste0("Excess deaths in ", LA, " during the pandemic"),
        subtitle=paste0("Weekly deaths in <span style='color:red;'>2020</span> compared to <span style='color:Skyblue4;'>the average in 2015-19</span> by date of occurance<br>Data up to ", enddate),
-       caption="Data from ONS | Plot by @VictimOfMaths")
+       caption=paste0("Data from ", source," | Plot by @VictimOfMaths"))
 
 #Excess deaths by cause
 ggplot(subset(data_bar, name==LA), aes(x=week, y=deaths, fill=excesscause))+
@@ -216,7 +307,7 @@ ggplot(subset(data_bar, name==LA), aes(x=week, y=deaths, fill=excesscause))+
   theme_classic()+
   labs(title=paste0("Excess deaths in ", LA, " during the pandemic"),
        subtitle=paste0("Excess deaths by date of occurance in 2020 vs. 2015-19 average by cause\nData up to ", enddate),
-       caption="Data from ONS | Plot by @VictimOfMaths")
+       caption=paste0("Data from ", source," | Plot by @VictimOfMaths"))
 
 #Excess deaths by location
 ggplot(subset(data.loc, name==LA), aes(x=week, y=excess, fill=location))+
@@ -228,7 +319,7 @@ ggplot(subset(data.loc, name==LA), aes(x=week, y=excess, fill=location))+
   theme_classic()+
   labs(title=paste0("Excess deaths in ", LA, " during the pandemic"),
        subtitle=paste0("Excess deaths by place of death in 2020 vs. 2015-19 average by cause\nData up to ", enddate),
-       caption="Data from ONS | Plot by @VictimOfMaths")
+       caption=paste0("Data from ", source," | Plot by @VictimOfMaths"))
 
 #Cases vs. deaths
 ggplot()+
@@ -242,7 +333,7 @@ ggplot()+
   theme(plot.subtitle=element_markdown())+
   labs(title=paste0("Timeline of COVID-19 in ", LA),
        subtitle=paste0("Confirmed new COVID-19 <span style='color:#B25D91;'>cases</span> compared to <span style='color:#1BB6AF;'>deaths</span> by week of occurance<br>Data up to ", enddate),
-       caption="Data from ONS | Plot by @VictimOfMaths")
+       caption=paste0("Data from ", source," | Plot by @VictimOfMaths"))
 
 #Cases by pillar (experimental and for England only)
 ggplot()+
