871ab2ecbbca8287b2d4d91fd66486f291efd61c
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-08-05 21:44:48 +0100

    Update NRS Excess Deaths by Cause.R

--- All Cause Mortality/NRS Excess Deaths by Cause.R
+++ All Cause Mortality/NRS Excess Deaths by Cause.R
@@ -6,9 +6,9 @@ library(readxl)
 library(paletteer)
 
 temp <- tempfile()
-source <- "https://www.nrscotland.gov.uk/files//statistics/covid19/covid-deaths-data-week-30.xlsx"
+source <- "https://www.nrscotland.gov.uk/files//statistics/covid19/covid-deaths-data-week-31.xlsx"
 temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
-endcol <- "AF"
+endcol <- "AG"
 
 
 #Historic data for all locations
