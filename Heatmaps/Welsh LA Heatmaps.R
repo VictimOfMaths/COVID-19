ccbc48dba0becc23dfbbfaf7cae17839bc7c6ff8
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-08-12 22:33:24 +0100

    Update Welsh LA Heatmaps.R

--- Heatmaps/Welsh LA Heatmaps.R
+++ Heatmaps/Welsh LA Heatmaps.R
@@ -1,14 +1,31 @@
+rm(list=ls())
+
+library(tidyverse)
+library(curl)
+library(forcats)
+library(readxl)
+library(RcppRoll)
+library(cowplot)
+
+#Read in data
+temp <- tempfile()
+source <- "http://www2.nphs.wales.nhs.uk:8080/CommunitySurveillanceDocs.nsf/3dc04669c9e1eaa880257062003b246b/77fdb9a33544aee88025855100300cab/$FILE/Rapid%20COVID-19%20surveillance%20data.xlsx"
+temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
+data <- read_excel(temp, sheet=3)
+
+colnames(data) <- c("LA", "date", "cases", "totalcases", "totalcaserate", "tests", "totaltests")
+data$date <- as.Date(data$date)
+
 heatmap <- data %>%
   group_by(LA) %>%
   mutate(casesroll_avg=roll_mean(cases, 7, align="left", fill=0)) %>%
-  mutate(maxcaserate=max(casesroll_avg), maxcaseday=date[which(casesroll_avg==maxcaserate)][1],
-         allcases=max(totalcases))
+  mutate(maxcaserate=max(casesroll_avg), maxcaseday=date[which(casesroll_avg==maxcaserate)][1])
 
 heatmap$maxcaseprop <- heatmap$casesroll_avg/heatmap$maxcaserate
 
 #Enter dates to plot from and to
 plotfrom <- "2020-03-01"
-plotto <- max(heatmap$date)
+plotto <- "2020-08-11"
 
 #Plot case trajectories
 casetiles <- ggplot(heatmap, aes(x=date, y=fct_reorder(LA, maxcaseday), fill=maxcaseprop))+
@@ -18,32 +35,19 @@ casetiles <- ggplot(heatmap, aes(x=date, y=fct_reorder(LA, maxcaseday), fill=max
   scale_y_discrete(name="", expand=c(0,0))+
   scale_x_date(name="Date", limits=as.Date(c(plotfrom, plotto)), expand=c(0,0))+
   labs(title="Timelines for COVID-19 cases in Welsh Local Authorities",
-       subtitle=paste0("The heatmap represents the 7-day rolling average of the number of new confirmed cases, normalised to the maximum value within the Local Authority.\nLAs are ordered by the date at which they reached their peak number of new cases. Bars on the right represent the absolute number of cases in each LA.\nData updated to ", plotto,". Data for most recent days is provisional and may be revised upwards as additional tests are processed."),
+       subtitle="The heatmap represents the 7-day rolling average of the number of new confirmed cases, normalised to the maximum value within the Local Authority.\nLAs are ordered by the date at which they reached their peak number of new cases. Bars on the right represent the absolute number of cases in each LA.\nData updated to 11th May. Data for most recent days is provisional and may be revised upwards as additional tests are processed.",
        caption="Data from Public Health Wales | Plot by @VictimOfMaths")+
   theme(axis.line.y=element_blank(), plot.subtitle=element_text(size=rel(0.78)), plot.title.position="plot",
         axis.text.y=element_text(colour="Black"))
 
-casebars <- ggplot(subset(heatmap, date==maxcaseday), aes(x=allcases, y=fct_reorder(LA, maxcaseday), fill=allcases))+
+casebars <- ggplot(subset(heatmap, date==maxcaseday), aes(x=totalcases, y=fct_reorder(LA, maxcaseday), fill=totalcases))+
   geom_col(show.legend=FALSE)+
   theme_classic()+
   scale_fill_distiller(palette="Spectral")+
-  scale_x_continuous(name="Total confirmed cases", breaks=c(0,500,1000,1500, 2000))+
+  scale_x_continuous(name="Total confirmed cases", breaks=c(0,500,1000))+
   theme(axis.title.y=element_blank(), axis.line.y=element_blank(), axis.text.y=element_blank(),
         axis.ticks.y=element_blank(), axis.text.x=element_text(colour="Black"))
 
 tiff("Outputs/COVIDWelshLACasesHeatmap.tiff", units="in", width=10, height=6, res=500)
 plot_grid(casetiles, casebars, align="h", rel_widths=c(1,0.2))
-dev.off()
-
-library(ggridges)
-
-tiff("Outputs/COVIDWelshLACaseRidges.tiff", units="in", width=10, height=6, res=500)
-ggplot(heatmap, aes(x=date, y=fct_reorder(LA, allcases), height=casesroll_avg, fill=casesroll_avg))+
-  geom_density_ridges_gradient(stat="identity")+
-  theme_classic()+
-  scale_fill_distiller(palette="Spectral", name="Cases per day\n7-day rolling avg.")+
-  scale_x_date(name="Date", limits=as.Date(c(plotfrom, plotto)), expand=c(0,0))+
-  scale_y_discrete(name="")+
-  labs(title="Timelines of confirmed COVID-19 cases in Welsh Local Authorities",
-       caption="Data from Public Health Wales | Plot by @VictimOfMaths")
 dev.off()
