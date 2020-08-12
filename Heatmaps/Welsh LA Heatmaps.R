10b4de0e1f2f778353fb4bb958ef6b1007caaec0
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-08-12 22:37:45 +0100

    Update Welsh LA Heatmaps.R

--- Heatmaps/Welsh LA Heatmaps.R
+++ Heatmaps/Welsh LA Heatmaps.R
@@ -51,3 +51,16 @@ casebars <- ggplot(subset(heatmap, date==maxcaseday), aes(x=totalcases, y=fct_re
 tiff("Outputs/COVIDWelshLACasesHeatmap.tiff", units="in", width=10, height=6, res=500)
 plot_grid(casetiles, casebars, align="h", rel_widths=c(1,0.2))
 dev.off()
+
+library(ggridges)
+
+tiff("Outputs/COVIDWelshLACaseRidges.tiff", units="in", width=10, height=6, res=500)
+ggplot(heatmap, aes(x=date, y=fct_reorder(LA, totalcases), height=casesroll_avg, fill=casesroll_avg))+
+  geom_density_ridges_gradient(stat="identity")+
+  theme_classic()+
+  scale_fill_distiller(palette="Spectral", name="Cases per day\n7-day rolling avg.")+
+  scale_x_date(name="Date", limits=as.Date(c(plotfrom, plotto)), expand=c(0,0))+
+  scale_y_discrete(name="")+
+  labs(title="Timelines of confirmed COVID-19 cases in Welsh Local Authorities",
+       caption="Data from Public Health Wales | Plot by @VictimOfMaths")
+dev.off()
