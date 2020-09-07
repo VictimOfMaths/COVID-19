efe2b83be6aee5bcb6c7f830384cef2ec812c44b
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-09-07 12:01:59 +0100

    Update COVIDPHESurveillance.R

--- Heatmaps/COVIDPHESurveillance.R
+++ Heatmaps/COVIDPHESurveillance.R
@@ -14,17 +14,17 @@ library(paletteer)
 #than the data relates to. Numbers here relate to actual weeks of data.
 
 #As of 28th August, PHE are actually publishing a time series of case numbers by age and sex
-temp <- tempfile()
-source <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/914814/Weekly_COVID19_report_data_w36.xlsx"
-temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
-case.m <- read_excel(temp, sheet="Figure 2b Cases by age and sex ", range="B12:L36", 
+temp1 <- tempfile()
+source1 <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/914814/Weekly_COVID19_report_data_w36.xlsx"
+temp1 <- curl_download(url=source1, destfile=temp1, quiet=FALSE, mode="wb")
+case.m <- read_excel(temp1, sheet="Figure 2b Cases by age and sex ", range="B12:L36", 
                      col_names=FALSE)
 colnames(case.m) <- c("Week", "0-4", "5-9", "10-19", "20-29", "30-39", "40-49", 
                           "50-59", "60-69", "70-79", "80+")
 case.m$sex <- "Male"
 case.m <- gather(case.m, age, cases, c(2:11))
 
-case.f <- read_excel(temp, sheet="Figure 2b Cases by age and sex ", range="B41:L65", 
+case.f <- read_excel(temp1, sheet="Figure 2b Cases by age and sex ", range="B41:L65", 
                      col_names=FALSE)
 colnames(case.f) <- c("Week", "0-4", "5-9", "10-19", "20-29", "30-39", "40-49", 
                       "50-59", "60-69", "70-79", "80+")
@@ -34,18 +34,11 @@ case.f <- gather(case.f, age, cases, c(2:11))
 cases <- bind_rows(case.m, case.f)
 
 #Bring in LA populations
-temp <- tempfile()
-source <- "https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fpopulationandmigration%2fpopulationestimates%2fdatasets%2fpopulationestimatesforukenglandandwalesscotlandandnorthernireland%2fmid20182019laboundaries/ukmidyearestimates20182019ladcodes.xls"
-temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
-pop.m <- as.data.frame(t(read_excel(temp, sheet="MYE2 - Males", range="E9:CQ9", col_names=FALSE)))
-pop.m$age <- c(0:90)
-pop.m$sex <- "Male"
-pop.f <- as.data.frame(t(read_excel(temp, sheet="MYE2 - Females", range="E9:CQ9", col_names=FALSE)))
-pop.f$age <- c(0:90)
-pop.f$sex <- "Female"
-
-pop <- bind_rows(pop.m, pop.f)
-
+temp2 <- tempfile()
+source2 <- "https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fpopulationandmigration%2fpopulationestimates%2fdatasets%2fpopulationestimatesforukenglandandwalesscotlandandnorthernireland%2fmid20182019laboundaries/ukmidyearestimates20182019ladcodes.xls"
+temp2 <- curl_download(url=source2, destfile=temp2, quiet=FALSE, mode="wb")
+pop <- as.data.frame(t(read_excel(temp2, sheet="MYE2-All", range="E9:CQ9", col_names=FALSE)))
+pop$age <- c(0:90)
 pop$ageband <- case_when(
   pop$age<5 ~ "0-4",
   pop$age<10 ~ "5-9",
@@ -60,10 +53,10 @@ pop$ageband <- case_when(
 )
 
 pop <- pop %>% 
-  group_by(ageband, sex) %>% 
+  group_by(ageband) %>% 
   summarise(pop=sum(V1))
 
-cases <- merge(cases, pop, by.x=c("age", "sex"), by.y=c("ageband", "sex"), all.x=TRUE)
+cases <- merge(cases, pop, by.x="age", by.y="ageband", all.x=TRUE)
 
 cases$cases <- if_else(cases$cases=="*", "0", cases$cases)
 cases$cases <- if_else(cases$cases=="-", "0", cases$cases)
@@ -74,6 +67,9 @@ cases$age <- factor(cases$age, levels=c("0-4", "5-9", "10-19", "20-29", "30-39",
                                         "40-49", "50-59", "60-69", "70-79", "80+"))
 
 #Heatmaps of cases by age: rates
+###############################
+#Need to split populations out by sex as there are currently wrong
+###############################
 tiff("Outputs/COVIDNewCasesHeatmap.tiff", units="in", width=13, height=4, res=500)
 ggplot(cases)+
   geom_tile(aes(x=Week, y=age, fill=caserate))+
@@ -133,4 +129,61 @@ cases %>%
   labs(title="The rise in COVID-19 cases is largely among 20-somethings",
        subtitle="Confirmed new cases in England by age band",
        caption="Date from PHE | Plot by @VictimOfMaths")
+dev.off()
+
+#Analysis of positivity data
+#Overall by pillar
+pos.pillars <- read_excel(temp1, sheet="Figure 1. Pillar 1+2 epicurve", range="B9:F39",
+                          col_names=FALSE)
+colnames(pos.pillars) <- c("Week", "P1cases", "P2cases", "P1pos", "P2pos")
+
+pos.pillars <- gather(pos.pillars, Pillar, posrate, c(4,5))
+pos.pillars$posrate <- as.numeric(if_else(pos.pillars$posrate=="-", "NA", pos.pillars$posrate))
+
+tiff("Outputs/COVIDPillarsPosRate.tiff", units="in", width=8, height=6, res=500)
+ggplot(pos.pillars, aes(x=Week, y=posrate/100, colour=Pillar))+
+  geom_line(show.legend=FALSE)+
+  scale_colour_paletteer_d("NineteenEightyR::malibu")+
+  scale_y_continuous(name="Proportion of tests which are positive", 
+                     labels = scales::percent_format(accuracy = 1))+  
+  theme_classic()+
+  theme(plot.subtitle=element_markdown())+
+  labs(title="The positivity rate of Pillar 2 tests is rising",
+       subtitle="Weekly positivity rates for <span style='color:#FF4E86;'>Pillar 1</span> and <span style='color:#FF9E44;'>Pillar 2 </span>tests in England",
+       caption="Date from PHE | Visualisation by @VictimOfMaths")
+dev.off()
+
+#By age and sex
+pos.age.m <- read_excel(temp1, sheet="Figure 6. Positivity by agegrp", range="B78:I108",
+                        col_names=FALSE)
+colnames(pos.age.m) <- c("Week", "0-4", "5-14", "15-44", "45-64", "65-74", "75-84", "85+")
+pos.age.m <- gather(pos.age.m, age, posrate, c(2:8))
+pos.age.m$posrate <- as.numeric(if_else(pos.age.m$posrate=="-", "NA", pos.age.m$posrate))
+pos.age.m$sex <- "Male"
+
+pos.age.f <- read_excel(temp1, sheet="Figure 6. Positivity by agegrp", range="B112:I142",
+                        col_names=FALSE)
+colnames(pos.age.f) <- c("Week", "0-4", "5-14", "15-44", "45-64", "65-74", "75-84", "85+")
+pos.age.f <- gather(pos.age.f, age, posrate, c(2:8))
+pos.age.f$posrate <- as.numeric(if_else(pos.age.f$posrate=="-", "NA", pos.age.f$posrate))
+pos.age.f$sex <- "Female"
+
+pos.age <- bind_rows(pos.age.m, pos.age.f)
+
+pos.age$age <- factor(pos.age$age, levels=c("0-4", "5-14", "15-44", "45-64", "65-74",
+                                            "75-84", "85+"))
+
+tiff("Outputs/COVIDPosRatexAge.tiff", units="in", width=10, height=6, res=500)
+ggplot(pos.age, aes(x=Week, y=posrate/100, colour=age))+
+  geom_line()+
+  scale_colour_paletteer_d(pal="awtools::a_palette",name="Age")+
+  scale_y_continuous(name="Proportion of tests which are positive", 
+                     labels = scales::percent_format(accuracy = 1))+  
+  xlim(c(18,max(pos.age$Week)+1))+
+  facet_wrap(~sex)+
+  theme_classic()+
+  theme(strip.background=element_blank(), strip.text=element_text(face="bold", size=rel(1)))+
+  labs(title="The positivity rate of tests is rising in almost all age groups",
+       subtitle="Weekly positivity rates for Pillar 2 tests in England by age group",
+       caption="Date from PHE | Visualisation by @VictimOfMaths")
 dev.off()
