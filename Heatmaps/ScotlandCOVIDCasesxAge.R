b02388388fe9a4349c65842022f8cfbf43bc57a2
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-09-18 17:14:35 +0100

    Update ScotlandCOVIDCasesxAge.R

--- Heatmaps/ScotlandCOVIDCasesxAge.R
+++ Heatmaps/ScotlandCOVIDCasesxAge.R
@@ -7,10 +7,11 @@ library(ggtext)
 library(ggridges)
 library(paletteer)
 library(ggstream)
+library(RcppRoll)
 
 #Scottish age data
 temp <- tempfile()
-source <- "https://www.opendata.nhs.scot/dataset/b318bddf-a4dc-4262-971f-0ba329e09b87/resource/9393bd66-5012-4f01-9bc5-e7a10accacf4/download/trend_agesex_20200912.csv"
+source <- "https://www.opendata.nhs.scot/dataset/b318bddf-a4dc-4262-971f-0ba329e09b87/resource/9393bd66-5012-4f01-9bc5-e7a10accacf4/download/trend_agesex_20200918.csv"
 temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
 
 data <- read.csv(temp)
@@ -59,6 +60,13 @@ data <- merge(data, pop, by=c("Sex", "AgeGroup"), all.x=TRUE)
 
 data$posrate <- data$DailyPositive*100000/data$pop
 
+#Take rolling 7-day averages
+data <- data %>% 
+  group_by(Sex, AgeGroup) %>% 
+  arrange(date) %>% 
+  mutate(cases_avg=roll_mean(DailyPositive, 7, align="right", fill=0),
+         posrate_avg=roll_mean(posrate, 7, align="right", fill=0))
+
 tiff("Outputs/COVIDCasesStreamgraphScotlandxSex.tiff", units="in", width=10, height=6, res=500)
 ggplot(subset(data, Sex!="Total"), aes(x=date, y=DailyPositive, fill=AgeGroup))+
   geom_stream(bw=0.2)+
@@ -93,7 +101,7 @@ dev.off()
 #Heatmap
 tiff("Outputs/COVIDCasesHeatmapScotland.tiff", units="in", width=10, height=3, res=500)
 ggplot(subset(data, Sex=="Total" & date>=as.Date("2020-07-01") & date<max(data$date)), 
-       aes(x=date, y=AgeGroup, fill=DailyPositive))+
+       aes(x=date, y=AgeGroup, fill=cases_avg))+
   geom_tile()+
   scale_x_date(name="")+
   scale_y_discrete(name="Age group",
@@ -101,49 +109,57 @@ ggplot(subset(data, Sex=="Total" & date>=as.Date("2020-07-01") & date<max(data$d
   scale_fill_paletteer_c("viridis::magma", name="New cases")+
   theme_classic()+
   labs(title="The rise in new COVID-19 cases in Scotland hasn't affected pensioners yet",
-       subtitle="Confirmed daily new cases in Scotland by age",
+       subtitle="Rolling 7-day average of daily confirmed new cases in Scotland by age",
        caption="Date from Public Health Scotland | Plot by @VictimOfMaths")
 dev.off()
 
 tiff("Outputs/COVIDCasesHeatmapScotlandRate.tiff", units="in", width=10, height=3, res=500)
 ggplot(subset(data, Sex=="Total" & date>=as.Date("2020-07-01") & date<max(data$date)), 
-       aes(x=date, y=AgeGroup, fill=posrate))+
+       aes(x=date, y=AgeGroup, fill=posrate_avg))+
   geom_tile()+
   scale_x_date(name="")+
   scale_y_discrete(name="Age group",
                    labels=c("15-19", "20-24", "25-44", "45-64", "65-74", "75-84", "85+"))+
   scale_fill_paletteer_c("viridis::magma", name="New cases\nper 100,000")+
   theme_classic()+
-  labs(title="The rise in new COVID-19 cases in Scotland hasn't affected pensioners yet",
+  labs(title="The rise in new COVID-19 cases in Scotland hasn't significantly affected pensioners yet",
        subtitle="Confirmed daily new case rates per 100,000 in Scotland by age",
        caption="Date from Public Health Scotland | Plot by @VictimOfMaths")
 dev.off()
 
 #By deprivation
 temp <- tempfile()
-source <- "https://www.opendata.nhs.scot/dataset/b318bddf-a4dc-4262-971f-0ba329e09b87/resource/a38a4c21-7c75-4ecd-a511-3f83e0e8f0c3/download/trend_simd_20200912.csv"
+source <- "https://www.opendata.nhs.scot/dataset/b318bddf-a4dc-4262-971f-0ba329e09b87/resource/a38a4c21-7c75-4ecd-a511-3f83e0e8f0c3/download/trend_simd_20200918.csv"
 temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
 
 data.simd <- read.csv(temp)
 
 data.simd <- data.simd %>% mutate(date=as.Date(as.character(Date), format="%Y%m%d"))
 
+#Take rolling 7-day averages
+data.simd <- data.simd %>% 
+  group_by(SIMDQuintile) %>% 
+  arrange(date) %>% 
+  mutate(cases_avg=roll_mean(DailyPositive, 7, align="right", fill=0),
+         deaths_avg=roll_mean(DailyDeaths, 7, align="right", fill=0))
+
+
 tiff("Outputs/COVIDCasesHeatmapScotlandxIMD.tiff", units="in", width=10, height=3, res=500)
 ggplot(subset(data.simd, date>=as.Date("2020-07-01") & date<max(data$date)), 
-       aes(x=date, y=as.factor(SIMDQuintile), fill=DailyPositive))+
+       aes(x=date, y=as.factor(SIMDQuintile), fill=cases_avg))+
   geom_tile()+
   scale_x_date(name="")+
   scale_y_discrete(name="Deprivation quintile",
                    labels=c("1 - most deprived", "2", "3", "4", "5 - least deprived"))+
   scale_fill_paletteer_c("viridis::magma", name="New cases")+
   theme_classic()+
-  labs(title="More deprived parts of Scotland aren't seeing bigger rises in COVID-19 cases",
-       subtitle="Confirmed daily new cases in Scotland by quintiles of the Scottish Index of Multiple Deprivation",
+  labs(title="The most deprived areas in Scotland now have slightly higher rates of new COVID-19 cases",
+       subtitle="Rolling 7-day average of confirmed daily new cases in Scotland by quintiles of the Scottish Index of Multiple Deprivation",
        caption="Date from Public Health Scotland | Plot by @VictimOfMaths")
 dev.off()
 
 tiff("Outputs/COVIDDeathsHeatmapScotlandxIMD.tiff", units="in", width=10, height=3, res=500)
-ggplot(data.simd, aes(x=date, y=as.factor(SIMDQuintile), fill=DailyDeaths))+
+ggplot(data.simd, aes(x=date, y=as.factor(SIMDQuintile), fill=deaths_avg))+
   geom_tile()+
   scale_x_date(name="")+
   scale_y_discrete(name="Deprivation quintile",
@@ -151,6 +167,7 @@ ggplot(data.simd, aes(x=date, y=as.factor(SIMDQuintile), fill=DailyDeaths))+
   scale_fill_paletteer_c("viridis::magma", name="Deaths per day")+
   theme_classic()+
   labs(title="Deaths from confirmed COVID-19 in Scotland were concentrated in more deprived areas",
-       subtitle="Confirmed daily deaths in Scotland by quintiles of the Scottish Index of Multiple Deprivation",
+       subtitle="Rolling 7-day average of confirmed daily new cases in Scotland by quintiles of the Scottish Index of Multiple Deprivation",
        caption="Date from Public Health Scotland | Plot by @VictimOfMaths")
 dev.off()
+
