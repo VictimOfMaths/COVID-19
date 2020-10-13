e5171537acb8f750a13248328b620520f2201894
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-10-13 17:19:57 +0100

    Update AllCauseDeaths.R

--- All Cause Mortality/AllCauseDeaths.R
+++ All Cause Mortality/AllCauseDeaths.R
@@ -11,24 +11,24 @@ library(ggtext)
 #A gold star to anyone who can make the range updates for the 3 different Excel files for E&W, Scotland & NI automatic.
 
 #Latest date in the country-specific data
-EWDate <- "25th September"
+EWDate <- "2nd October"
 ScotDate <- "27th September"
-NIDate <- "25th September"
+NIDate <- "2nd October"
 
 #Locations for latest data. Links for historical data don't move, so keep them further down
-Eng2020 <- "https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fbirthsdeathsandmarriages%2fdeaths%2fdatasets%2fweeklyprovisionalfiguresondeathsregisteredinenglandandwales%2f2020/publishedweek392020.xlsx"
+Eng2020 <- "https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fbirthsdeathsandmarriages%2fdeaths%2fdatasets%2fweeklyprovisionalfiguresondeathsregisteredinenglandandwales%2f2020/publishedweek402020.xlsx"
 Scot2020 <- "https://www.nrscotland.gov.uk/files//statistics/covid19/covid-deaths-data-week-37.xlsx"
 #https://data.gov.scot/coronavirus-covid-19/data.html
-Scot2020v2 <- "https://data.gov.scot/coronavirus-covid-19/download/Scottish%20Government%20COVID-19%20data%20(07%20October%202020).xlsx"
+Scot2020v2 <- "https://data.gov.scot/coronavirus-covid-19/download/Scottish%20Government%20COVID-19%20data%20(12%20October%202020).xlsx"
 NI2020 <- "https://www.nisra.gov.uk/sites/nisra.gov.uk/files/publications/Weekly_Deaths.xls"
 
 #Stupid Excel range controls
-EngRange <- "AO" #increment by one letter each week
+EngRange <- "AP" #increment by one letter each week
 ScotRange <- "AM" #
 #These next two bookend the range for the weeks inbetween NRS's now monthly reports
 ScotRangev2.1 <- "42" #update after each new monthly report
-ScotRangev2.2 <- "44" #increment by one number each week
-NIRange <- "43" #increment by one number each week
+ScotRangev2.2 <- "45" #increment by one number each week
+NIRange <- "44" #increment by one number each week
 
 #Flag weeks with an NRS report
 NRSweek <- FALSE
@@ -896,7 +896,7 @@ ggplot()+
   scale_x_continuous(name="Week number", breaks=c(0,10,20,30,40,50))+
   scale_y_continuous(name="Deaths registered")+
   expand_limits(y=0)+
-  labs(title="The slight rise in deaths seems concentrated in 75-84 year-old men",
+  labs(title="The slight rise in deaths seems greatest in 75-84 year-old men",
        subtitle=paste0("Weekly deaths in <span style='color:red;'>2020</span> compared to <span style='color:Skyblue4;'>the range in 2010-19</span>. Data up to ", EWDate, "."),
        caption="Data from ONS | Plot by @VictimOfMaths")+
   theme(strip.background=element_blank(), strip.text=element_text(face="bold", size=rel(1)),
@@ -1149,7 +1149,7 @@ RegPlot <- ggplot()+
   scale_x_continuous(name="Week number", breaks=c(0,10,20,30,40,50))+
   scale_y_continuous(name="Deaths registered")+
   expand_limits(y=0)+
-  labs(title="All-cause deaths are above 'normal' levels in the South-East and West Midlands",
+  labs(title="Regional variation in all-cause mortality in the UK",
        subtitle=subtitle,
        caption="Data from ONS, NRS & NISRA | Plot by @VictimOfMaths")+
   theme(strip.background=element_blank(), strip.text=element_text(face="bold", size=rel(1)),
@@ -1270,6 +1270,7 @@ temp26 <- as.data.frame(t(read_excel(temp, sheet=11, range="EV9:EV14", col_names
 temp27 <- as.data.frame(t(read_excel(temp, sheet=11, range="FB9:FB14", col_names=FALSE)))
 temp28 <- as.data.frame(t(read_excel(temp, sheet=11, range="FH9:FH14", col_names=FALSE)))
 temp29 <- as.data.frame(t(read_excel(temp, sheet=11, range="FN9:FN14", col_names=FALSE)))
+temp30 <- as.data.frame(t(read_excel(temp, sheet=11, range="FT9:FT14", col_names=FALSE)))
 
 colnames(temp1) <- temp1 %>% slice(1) %>% unlist()
 temp1 <- temp1 %>% slice(-1)
@@ -1278,7 +1279,8 @@ temp1 <- temp1 %>% mutate_if(is.factor, as.character) %>% mutate_if(is.character
 
 data20 <- bind_rows(temp2, temp3, temp4, temp5, temp6, temp7, temp8, temp9, temp10, temp11, 
                     temp12, temp13, temp14, temp15, temp16, temp17, temp18, temp19, temp20,
-                    temp21, temp22, temp23, temp24, temp25, temp26, temp27, temp28, temp29)
+                    temp21, temp22, temp23, temp24, temp25, temp26, temp27, temp28, temp29,
+                    temp30)
 
 data20$week <- c(12:EWmaxweek)
 
@@ -1345,7 +1347,7 @@ ggplot()+
   scale_fill_paletteer_d("LaCroixColoR::PinaFraise", name="Cause", labels=c("COVID-19", "Other causes"))+
   scale_colour_manual(values="NavyBlue", name="", labels="Net excess deaths")+
   theme_classic()+
-  labs(title="Confirmed COVID-19 deaths have increased, but are still relatively low",
+  labs(title="Confirmed COVID-19 deaths have increased, but are still relatively well below the April/May peak",
        subtitle="Excess deaths vs. 2015-19 average by cause for England & Wales",
        caption="Data from ONS | Plot by @VictimOfMaths")
 dev.off()
