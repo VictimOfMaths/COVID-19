71f26bd5aa6c8cc0c82d76ac2251cc515356c0da
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-10-26 10:11:06 +0000

    Updated to pull data from API

--- Heatmaps/COVIDCycle.R
+++ Heatmaps/COVIDCycle.R
@@ -3,45 +3,74 @@ rm(list=ls())
 library(tidyverse)
 library(curl)
 library(lubridate)
+library(ukcovid19) #remotes::install_github("publichealthengland/coronavirus-dashboard-api-R-sdk")
 library(paletteer)
 library(ggtext)
 
-temp <- tempfile()
-source <- "https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newAdmissions%22:%22newAdmissions%22,%22cumAdmissions%22:%22cumAdmissions%22%7D&format=csv"
-temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
-admissions <- read.csv(temp)
+options(scipen=9999)
+
+admissions <- get_data(filters="areaType=nation", structure=list(date="date",
+                                                             name="areaName",
+                                                             admissions="newAdmissions"))
 
 admissions <- admissions %>% 
-  filter(areaName=="England") %>% 
   mutate(date=as.Date(date)) %>% 
   filter(date>as.Date("2020-03-19"))
 
-
-temp <- tempfile()
-source <- "https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation&structure=%7B%22areaType%22:%22areaType%22,%22areaName%22:%22areaName%22,%22areaCode%22:%22areaCode%22,%22date%22:%22date%22,%22newDeaths28DaysByDeathDate%22:%22newDeaths28DaysByDeathDate%22,%22cumDeaths28DaysByDeathDate%22:%22cumDeaths28DaysByDeathDate%22%7D&format=csv"
-temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
-deaths <- read.csv(temp)
+deaths <- get_data(filters="areaType=nation", structure=list(date="date",
+                                                             name="areaName",
+                                                             deaths="newDeaths28DaysByDeathDate"))
 
 deaths <- deaths %>% 
-  filter(areaName=="England") %>% 
   mutate(date=as.Date(date)) %>% 
   filter(date>as.Date("2020-03-19"))
 
 data <- merge(admissions, deaths) %>% 
-  mutate(days=yday(date), weekno=week(date))
+  mutate(days=yday(date), weekno=week(date), pop=case_when(
+    name=="England" ~ 56286961,
+    name=="Northern Ireland" ~ 1893667,
+    name=="Scotland" ~ 5463300,
+    name=="Wales" ~ 3152879),
+    admrate=admissions*100000/pop, mortrate=deaths*100000/pop) %>% 
+  arrange(date)
 
 weekdata <- data %>% 
-  group_by(weekno) %>% 
-  summarise(admissions=mean(newAdmissions), deaths=mean(newDeaths28DaysByDeathDate)) %>% 
-  mutate(label=as.Date("2020-01-01")+days(weekno)*7-1, label=format(label, "%d %b"))
+  group_by(weekno, name) %>% 
+  summarise(admissions=mean(admissions), deaths=mean(deaths),
+            admrate=mean(admrate), mortrate=mean(mortrate)) %>% 
+  mutate(label=as.Date("2020-01-01")+days(weekno)*7-1, label=format(label, "%d %b")) %>% 
+  ungroup() %>% 
+  arrange(weekno)
 
-tiff("Outputs/COVIDCycle.tiff", units="in", width=8, height=6, res=500)
+#Faceted plot for UK nations
+tiff("Outputs/COVIDCycleUK.tiff", units="in", width=12, height=8, res=500)
 ggplot()+
-  geom_path(data=data, aes(x=newAdmissions, y=newDeaths28DaysByDeathDate), alpha=0.1,
+  geom_path(data=data, aes(x=admrate, y=mortrate), alpha=0.1,
             arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
-  geom_path(data=weekdata, aes(x=admissions, y=deaths), colour="tomato",
+  geom_path(data=weekdata, aes(x=admrate, y=mortrate), colour="tomato",
             arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
-  geom_text(data=weekdata, aes(x=admissions, y=deaths, label=label), size=rel(2), colour="Grey40",
+  geom_text(data=weekdata, aes(x=admrate, y=mortrate, label=label), size=rel(2), colour="Grey40",
+            vjust=-0.4)+
+  scale_x_continuous(trans="log10", name="Daily COVID-19 admissions per 100,000 (log scale)")+
+  scale_y_continuous(trans="log10", name="Daily COVID-19 deaths per 100,000 (log scale)")+
+  facet_wrap(~name)+
+  theme_classic()+
+  theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold"),
+        strip.background=element_blank(), strip.text=element_text(face="bold", size=rel(1)))+
+  labs(title="Breaking the cycle",
+       subtitle="COVID-19 hospital admissions and deaths across the UK <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
+       caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
+
+dev.off()
+
+#Plot for England only
+tiff("Outputs/COVIDCycleEng.tiff", units="in", width=8, height=6, res=500)
+ggplot()+
+  geom_path(data=subset(data, name=="England"), aes(x=admissions, y=deaths), alpha=0.1,
+            arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
+  geom_path(data=subset(weekdata, name=="England"), aes(x=admissions, y=deaths), colour="tomato",
+            arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
+  geom_text(data=subset(weekdata, name=="England"), aes(x=admissions, y=deaths, label=label), size=rel(2), colour="Grey40",
             vjust=-0.4)+
   scale_x_continuous(trans="log10", name="Daily COVID-19 admissions (log scale)")+
   scale_y_continuous(trans="log10", name="Daily COVID-19 deaths (log scale)")+
@@ -50,4 +79,63 @@ ggplot()+
   labs(title="Breaking the cycle",
        subtitle="COVID-19 hospital admissions and deaths in England <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
+
 dev.off()
+
+#Plot for Wales only
+tiff("Outputs/COVIDCycleWal.tiff", units="in", width=8, height=6, res=500)
+ggplot()+
+  geom_path(data=subset(data, name=="Wales"), aes(x=admissions, y=deaths), alpha=0.1,
+            arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
+  geom_path(data=subset(weekdata, name=="Wales"), aes(x=admissions, y=deaths), colour="tomato",
+            arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
+  geom_text(data=subset(weekdata, name=="Wales"), aes(x=admissions, y=deaths, label=label), size=rel(2), colour="Grey40",
+            vjust=-0.4)+
+  scale_x_continuous(trans="log10", name="Daily COVID-19 admissions (log scale)")+
+  scale_y_continuous(trans="log10", name="Daily COVID-19 deaths (log scale)")+
+  theme_classic()+
+  theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold"))+
+  labs(title="Breaking the cycle",
+       subtitle="COVID-19 hospital admissions and deaths in Wales <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
+       caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
+
+dev.off()
+
+#Plot for Scotland only
+tiff("Outputs/COVIDCycleSco.tiff", units="in", width=8, height=6, res=500)
+ggplot()+
+  geom_path(data=subset(data, name=="Scotland"), aes(x=admissions, y=deaths), alpha=0.1,
+            arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
+  geom_path(data=subset(weekdata, name=="Scotland"), aes(x=admissions, y=deaths), colour="tomato",
+            arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
+  geom_text(data=subset(weekdata, name=="Scotland"), aes(x=admissions, y=deaths, label=label), size=rel(2), colour="Grey40",
+            vjust=-0.4)+
+  scale_x_continuous(trans="log10", name="Daily COVID-19 admissions (log scale)")+
+  scale_y_continuous(trans="log10", name="Daily COVID-19 deaths (log scale)")+
+  theme_classic()+
+  theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold"))+
+  labs(title="Breaking the cycle",
+       subtitle="COVID-19 hospital admissions and deaths in Scotland <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
+       caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
+
+dev.off()
+
+#Plot for NI only
+tiff("Outputs/COVIDCycleNI.tiff", units="in", width=8, height=6, res=500)
+ggplot()+
+  geom_path(data=subset(data, name=="Northern Ireland"), aes(x=admissions, y=deaths), alpha=0.1,
+            arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
+  geom_path(data=subset(weekdata, name=="Northern Ireland"), aes(x=admissions, y=deaths), colour="tomato",
+            arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
+  geom_text(data=subset(weekdata, name=="Northern Ireland"), aes(x=admissions, y=deaths, label=label), size=rel(2), colour="Grey40",
+            vjust=-0.4)+
+  scale_x_continuous(trans="log10", name="Daily COVID-19 admissions (log scale)")+
+  scale_y_continuous(trans="log10", name="Daily COVID-19 deaths (log scale)")+
+  theme_classic()+
+  theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold"))+
+  labs(title="Breaking the cycle",
+       subtitle="COVID-19 hospital admissions and deaths in Northern Ireland <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
+       caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
+
+dev.off()
+
