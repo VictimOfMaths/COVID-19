fcf9f62f0c72a7307f3cefafa40cc4c0295b816e
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-10-26 11:42:46 +0000

    Update COVIDCycle.R

--- Heatmaps/COVIDCycle.R
+++ Heatmaps/COVIDCycle.R
@@ -139,3 +139,68 @@ ggplot()+
 
 dev.off()
 
+
+#Repeat at regional level
+admissions.reg <- get_data(filters="areaType=nhsregion", structure=list(date="date",
+                                                                 name="areaName",
+                                                                 admissions="newAdmissions"))
+
+admissions.reg <- admissions.reg %>% 
+  mutate(date=as.Date(date)) %>% 
+  filter(date>as.Date("2020-03-19"))
+
+deaths.reg <- get_data(filters="areaType=region", structure=list(date="date",
+                                                             name="areaName",
+                                                             deaths="newDeaths28DaysByDeathDate"))
+
+#Compress regions to align (more or less) with NHS regions
+deaths.reg <- deaths.reg %>% 
+  mutate(date=as.Date(date)) %>% 
+  filter(date>as.Date("2020-03-19")) %>% 
+  mutate(name=case_when(name %in% c("East Midlands", "West Midlands") ~ "Midlands",
+                        name %in% c("North East", "Yorkshire and The Humber") ~ "North East and Yorkshire",
+                        TRUE ~ name)) %>% 
+  group_by(date, name) %>% 
+  summarise(deaths=sum(deaths)) %>% 
+  ungroup()
+
+data.reg <- merge(admissions.reg, deaths.reg) %>% 
+  mutate(days=yday(date), weekno=week(date), pop=case_when(
+    name=="East of England" ~ 6236072,
+    name=="London" ~ 8961989,
+    name=="Midlands" ~ 4835928+5934037,
+    name=="North East and Yorkshire" ~ 2669941+5502967,
+    name=="North West" ~ 7341196,
+    name=="South East" ~ 9180135,
+    name=="South West" ~ 5624696),
+    admrate=admissions*100000/pop, mortrate=deaths*100000/pop) %>% 
+  arrange(date)
+
+weekdata.reg <- data.reg %>% 
+  group_by(weekno, name) %>% 
+  summarise(admissions=mean(admissions), deaths=mean(deaths),
+            admrate=mean(admrate), mortrate=mean(mortrate)) %>% 
+  mutate(label=as.Date("2020-01-01")+days(weekno)*7-1, label=format(label, "%d %b")) %>% 
+  ungroup() %>% 
+  arrange(weekno)
+
+#Faceted plot for UK nations
+tiff("Outputs/COVIDCycleReg.tiff", units="in", width=14, height=8, res=500)
+ggplot()+
+  geom_path(data=data.reg, aes(x=admrate, y=mortrate), alpha=0.1,
+            arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
+  geom_path(data=weekdata.reg, aes(x=admrate, y=mortrate), colour="tomato",
+            arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
+  #geom_text(data=weekdata.reg, aes(x=admrate, y=mortrate, label=label), size=rel(2), colour="Grey40",
+  #          vjust=-0.4)+
+  scale_x_continuous(trans="log10", name="Daily COVID-19 admissions per 100,000 (log scale)")+
+  scale_y_continuous(trans="log10", name="Daily COVID-19 deaths per 100,000 (log scale)")+
+  facet_wrap(~name)+
+  theme_classic()+
+  theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold"),
+        strip.background=element_blank(), strip.text=element_text(face="bold", size=rel(1)))+
+  labs(title="Breaking the cycle",
+       subtitle="COVID-19 hospital admissions and deaths across England <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average</span>.<br>Admissions data is published for NHS regions while deaths data is at government region level. These geographies are similar but may not overlap perfectly.",
+       caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
+
+dev.off()
