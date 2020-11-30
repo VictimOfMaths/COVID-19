450a6c605c6b6d526c5bb2d0f614a8d5044915f8
Colin Angus <c.r.angus@sheffield.ac.uk>
2020-11-30 16:27:14 +0000

    Update German State Heatmaps.R

--- Heatmaps/German State Heatmaps.R
+++ Heatmaps/German State Heatmaps.R
@@ -8,6 +8,8 @@ library(RcppRoll)
 library(zoo)
 library(cowplot)
 
+options(scipen=999999)
+
 #Read in data
 temp <- tempfile()
 source <- "https://raw.githubusercontent.com/jgehrcke/covid-19-germany-gae/master/data.csv"
@@ -38,6 +40,7 @@ data_long$State <- case_when(
   data_long$State=="DE.ST" ~ "Schleswig-Holstein",
   data_long$State=="DE.TH" ~ "Thüringen")
 
+
 #There appear to be weird errors in the data for Bayern (cases) and Hamburg (cases & deaths) 
 #from 1st September so remove these data points for now
 data_long <- data_long %>% 
@@ -62,6 +65,26 @@ fulldata <- fulldata %>%
   group_by(State, measure) %>%
   mutate(count=na.spline(count))
 
+#Add in populations
+fulldata <- fulldata %>% 
+  mutate(pop=case_when(
+    State=="Baden-Württemberg" ~ 11069533,
+    State=="Bayern" ~ 13076721,
+    State=="Brandenburg" ~ 2511917,
+    State=="Berlin" ~ 2644826,
+    State=="Bremen" ~ 682986,
+    State=="Hamburg" ~ 1841179,
+    State=="Hessen" ~ 6265809,
+    State=="Mecklenburg-Vorpommern" ~ 1609675,
+    State=="Niedersachsen" ~ 7982448,
+    State=="Nordrhein-Westfalen" ~ 17932651,
+    State=="Rheinland-Pfalz" ~ 4084844,
+    State=="Saarland" ~ 990509,
+    State=="Sachsen-Anhalt" ~ 2208321,
+    State=="Sachsen" ~ 4077937,
+    State=="Schleswig-Holstein" ~ 2896712,
+    State=="Thüringen" ~ 2143145))
+
 #Calculate daily figures
 fulldata <- fulldata %>%
   arrange(measure, State, date) %>%
@@ -75,10 +98,10 @@ fulldata$daycount <- ifelse(fulldata$daycount<0, 0, fulldata$daycount)
 heatmap <- fulldata %>%
   group_by(State, measure) %>%
   mutate(casesroll_avg=roll_mean(daycount, 7, align="right", fill=0)) %>%
-  mutate(maxcaserate=max(casesroll_avg), maxcaseday=date[which(casesroll_avg==maxcaserate)][1],
-         totalcases=max(count))
+  mutate(maxcases=max(casesroll_avg), maxcaseday=date[which(casesroll_avg==maxcases)][1],
+         totalcases=max(count), maxcaseprop=casesroll_avg/maxcases,
+         caserate=daycount*100000/pop, caserate_avg=casesroll_avg*100000/pop)
 
-heatmap$maxcaseprop <- heatmap$casesroll_avg/heatmap$maxcaserate
 
 #Enter dates to plot from and to
 plotfrom <- "2020-03-21"
@@ -110,6 +133,32 @@ tiff("Outputs/COVIDGermanStateCasesHeatmap.tiff", units="in", width=11, height=6
 plot_grid(casetiles, casebars, align="h", rel_widths=c(1,0.2))
 dev.off()
 
+#Plot case rate version
+caseratetiles <- ggplot(subset(heatmap, measure=="cases"), aes(x=date, y=fct_reorder(State, maxcaseday), fill=caserate_avg))+
+  geom_tile(colour="White", show.legend=FALSE)+
+  theme_classic()+
+  scale_fill_distiller(palette="Spectral")+
+  scale_y_discrete(name="", expand=c(0,0))+
+  scale_x_date(name="Date", limits=as.Date(c(plotfrom, plotto)), expand=c(0,0))+
+  labs(title="Timelines for COVID-19 case rates in German Bundesländer",
+       subtitle=paste0("The heatmap represents the 7-day rolling average of the rate of new confirmed cases per 100,000 population.\nStates are ordered by the date at which they reached their peak number of new cases. Bars on the right represent the population of each State.\nData updated to ", plotto,". Data for most recent days is provisional and may be revised upwards as additional tests are processed."),
+       caption="Data from Jan-Philip Gehrcke (https://covid19-germany.appspot.com/) | Plot by @VictimOfMaths")+
+  theme(axis.line.y=element_blank(), plot.subtitle=element_text(size=rel(0.78)), plot.title.position="plot",
+        axis.text.y=element_text(colour="Black"))
+
+caseratebars <- ggplot(subset(heatmap, date==maxcaseday & measure=="cases"), 
+                   aes(x=pop/1000000, y=fct_reorder(State, maxcaseday), fill=pop))+
+  geom_col(show.legend=FALSE)+
+  theme_classic()+
+  scale_fill_distiller(palette="Spectral")+
+  scale_x_continuous(name="Population\n(millions)")+
+  theme(axis.title.y=element_blank(), axis.line.y=element_blank(), axis.text.y=element_blank(),
+        axis.ticks.y=element_blank(), axis.text.x=element_text(colour="Black"))
+
+tiff("Outputs/COVIDGermanStateCaseRateHeatmap.tiff", units="in", width=11, height=6, res=500)
+plot_grid(caseratetiles, caseratebars, align="h", rel_widths=c(1,0.2))
+dev.off()
+
 #Plot death trajectories
 deathtiles <- ggplot(subset(heatmap, measure=="deaths"), aes(x=date, y=fct_reorder(State, maxcaseday), fill=maxcaseprop))+
   geom_tile(colour="White", show.legend=FALSE)+
@@ -134,6 +183,23 @@ deathbars <- ggplot(subset(heatmap, date==maxcaseday & measure=="deaths"),
 
 tiff("Outputs/COVIDGermanStateDeathsHeatmap.tiff", units="in", width=11, height=6, res=500)
 plot_grid(deathtiles, deathbars, align="h", rel_widths=c(1,0.2))
+dev.off()
+
+#Death rates
+deatratetiles <- ggplot(subset(heatmap, measure=="deaths"), aes(x=date, y=fct_reorder(State, maxcaseday), fill=caserate_avg))+
+  geom_tile(colour="White", show.legend=FALSE)+
+  theme_classic()+
+  scale_fill_distiller(palette="Spectral")+
+  scale_y_discrete(name="", expand=c(0,0))+
+  scale_x_date(name="Date", limits=as.Date(c(plotfrom, plotto)), expand=c(0,0))+
+  labs(title="Timelines for COVID-19 death rates in German Bundesländer",
+       subtitle=paste0("The heatmap represents the 7-day rolling average of the rate of new confirmed deaths per 100,000 population.\nStates are ordered by the date at which they reached their peak number of deaths. Bars on the right represent the population of each State.\nData updated to ", plotto,". Data for most recent days is provisional and may be revised upwards as additional tests are processed."),
+       caption="Data from Jan-Philip Gehrcke (https://covid19-germany.appspot.com/) | Plot by @VictimOfMaths")+
+  theme(axis.line.y=element_blank(), plot.subtitle=element_text(size=rel(0.78)), plot.title.position="plot",
+        axis.text.y=element_text(colour="Black"))
+
+tiff("Outputs/COVIDGermanStateDeathRateHeatmap.tiff", units="in", width=11, height=6, res=500)
+plot_grid(deatratetiles, caseratebars, align="h", rel_widths=c(1,0.2))
 dev.off()
 
 #Try them as ridgeplots
