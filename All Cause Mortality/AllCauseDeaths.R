353df3db06fbcb17b59aa6c7fb1105e3e2be1ead
Colin Angus <c.r.angus@sheffield.ac.uk>
2021-01-08 18:36:55 +0000

    Update AllCauseDeaths.R

--- All Cause Mortality/AllCauseDeaths.R
+++ All Cause Mortality/AllCauseDeaths.R
@@ -13,14 +13,14 @@ library(ggtext)
 #Latest date in the country-specific data
 EWDate <- "25th December"
 ScotDate <- "3rd January"
-NIDate <- "18th December"
+NIDate <- "1st January"
 
 #Locations for latest data. Links for historical data don't move, so keep them further down
 Eng2020 <- "https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fbirthsdeathsandmarriages%2fdeaths%2fdatasets%2fweeklyprovisionalfiguresondeathsregisteredinenglandandwales%2f2020/publishedweek522020.xlsx"
 Scot2020 <- "https://www.nrscotland.gov.uk/files//statistics/covid19/covid-deaths-data-week-53.xlsx"
 #https://data.gov.scot/coronavirus-covid-19/data.html
 Scot2020v2 <- "https://data.gov.scot/coronavirus-covid-19/download/Scottish%20Government%20COVID-19%20data%20(28%20October%202020).xlsx"
-NI2020 <- "https://www.nisra.gov.uk/sites/nisra.gov.uk/files/publications/Weekly_Deaths.xls"
+NI2020 <- "https://www.nisra.gov.uk/sites/nisra.gov.uk/files/publications/Weekly_Deaths.xlsx"
 
 #Stupid Excel range controls
 EngRange <- "BB" #increment by one letter each week
@@ -28,7 +28,7 @@ ScotRange <- "BC" #
 #These next two bookend the range for the weeks inbetween NRS's now monthly reports
 ScotRangev2.1 <- "46" #update after each new monthly report
 ScotRangev2.2 <- "47" #increment by one number each week
-NIRange <- "55" #increment by one number each week
+NIRange <- "57" #increment by one number each week
 
 #Flag weeks with an NRS report
 NRSweek <- TRUE
@@ -751,8 +751,8 @@ data.S <- data.S %>%
          year=if_else(year==2021, 2020, year))
 
 data.NI <- data.NI %>% 
-  mutate(weekno=if_else(year==2021 & weekno==1, 53, weekno),
-         year=if_else(year==2021, 2020, year))
+  mutate(weekno=if_else(year==2021 & weekno==1, 53, as.double(weekno)),
+         year=if_else(year==2021, 2020, as.double(year)))
 
 #Pull out latest week numbers for label placement
 EWmaxweek <- max(subset(data.reg.UK, reg=="London" & year==2020)$weekno)
@@ -1096,7 +1096,7 @@ ggplot()+
   scale_x_continuous(name="Week number", breaks=c(0,10,20,30,40,50))+
   scale_y_continuous(name="Deaths registered")+
   expand_limits(y=0)+
-  labs(title="Deaths in Northern Ireland are just within 'normal' levels",
+  labs(title="Deaths in Northern Ireland are still above average",
        subtitle=paste0("Weekly all-cause deaths in <span style='color:red;'>2020</span> compared to <span style='color:Skyblue4;'>the range in 2011-19</span>. Data up to ", NIDate, "."),
        caption="Data from NISRA | Plot by @VictimOfMaths")+
   annotate(geom="text", x=23, y=labpos, label=paste0(round(NI.excess$excess, 0), 
