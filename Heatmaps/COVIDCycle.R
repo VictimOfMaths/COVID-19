3a7c2fbba76b98a147322b21aa146eb7cc357042
Colin Angus <c.r.angus@sheffield.ac.uk>
2021-01-08 12:44:13 +0000

    Update COVIDCycle.R

--- Heatmaps/COVIDCycle.R
+++ Heatmaps/COVIDCycle.R
@@ -26,7 +26,7 @@ deaths <- deaths %>%
   filter(date>as.Date("2020-03-19"))
 
 data <- merge(admissions, deaths) %>% 
-  mutate(days=yday(date), weekno=week(date), pop=case_when(
+  mutate(days=yday(date), weekno=if_else(year(date)==2020, week(date), week(date)+52), pop=case_when(
     name=="England" ~ 56286961,
     name=="Northern Ireland" ~ 1893667,
     name=="Scotland" ~ 5463300,
@@ -57,29 +57,34 @@ ggplot()+
   theme_classic()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold"),
         strip.background=element_blank(), strip.text=element_text(face="bold", size=rel(1)))+
-  labs(title="Breaking the cycle",
-       subtitle="COVID-19 hospital admissions and deaths across the UK <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
+  labs(title="Going round in circles",
+       subtitle="New hospital admissions with positive COVID-19 test and deaths within 28 days of a positive test across the UK <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
 dev.off()
 
 #Plot for England only
-tiff("Outputs/COVIDCycleEng.tiff", units="in", width=8, height=6, res=500)
-ggplot()+
+EngCycle <- ggplot()+
   geom_path(data=subset(data, name=="England"), aes(x=admissions, y=deaths), alpha=0.1,
             arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
   geom_path(data=subset(weekdata, name=="England"), aes(x=admissions, y=deaths), colour="tomato",
             arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
   geom_text(data=subset(weekdata, name=="England"), aes(x=admissions, y=deaths, label=label), size=rel(2), colour="Grey40",
             vjust=-0.4)+
-  scale_x_continuous(trans="log10", name="Daily COVID-19 admissions (log scale)")+
-  scale_y_continuous(trans="log10", name="Daily COVID-19 deaths (log scale)")+
+ scale_x_continuous(trans="log10", name="Daily COVID-19 admissions (log scale)")+
+ scale_y_continuous(trans="log10", name="Daily COVID-19 deaths (log scale)")+
   theme_classic()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold"))+
-  labs(title="Breaking the cycle",
-       subtitle="COVID-19 hospital admissions and deaths in England <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
+  labs(title="Going round in circles",
+       subtitle="New hospital admissions with positive COVID-19 test and deaths within 28 days of a positive test<br>in England <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
+tiff("Outputs/COVIDCycleEng.tiff", units="in", width=8, height=6, res=500)
+EngCycle
+dev.off()
+
+png("Outputs/COVIDCycleEng.png", units="in", width=8, height=6, res=500)
+EngCycle
 dev.off()
 
 #Plot for Wales only
@@ -95,8 +100,8 @@ ggplot()+
   scale_y_continuous(trans="log10", name="Daily COVID-19 deaths (log scale)")+
   theme_classic()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold"))+
-  labs(title="Breaking the cycle",
-       subtitle="COVID-19 hospital admissions and deaths in Wales <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
+  labs(title="Going round in circles",
+       subtitle="New hospital admissions with positive COVID-19 test and deaths within 28 days of a positive test<br>in Wales <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
 dev.off()
@@ -114,8 +119,8 @@ ggplot()+
   scale_y_continuous(trans="log10", name="Daily COVID-19 deaths (log scale)")+
   theme_classic()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold"))+
-  labs(title="Breaking the cycle",
-       subtitle="COVID-19 hospital admissions and deaths in Scotland <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
+  labs(title="Going round in circles",
+       subtitle="New hospital admissions with positive COVID-19 test and deaths within 28 days of a positive test<br>in Scotland <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
 dev.off()
@@ -133,12 +138,11 @@ ggplot()+
   scale_y_continuous(trans="log10", name="Daily COVID-19 deaths (log scale)")+
   theme_classic()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold"))+
-  labs(title="Breaking the cycle",
-       subtitle="COVID-19 hospital admissions and deaths in Northern Ireland <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
+  labs(title="Going round in circles",
+       subtitle="New hospital admissions with positive COVID-19 test and deaths within 28 days of a positive test<br>in Northern Ireland <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
 dev.off()
-
 
 #Repeat at regional level
 admissions.reg <- get_data(filters="areaType=nhsregion", structure=list(date="date",
@@ -165,7 +169,7 @@ deaths.reg <- deaths.reg %>%
   ungroup()
 
 data.reg <- merge(admissions.reg, deaths.reg) %>% 
-  mutate(days=yday(date), weekno=week(date), pop=case_when(
+  mutate(days=yday(date), weekno=if_else(year(date)==2020, week(date), week(date)+52), pop=case_when(
     name=="East of England" ~ 6236072,
     name=="London" ~ 8961989,
     name=="Midlands" ~ 4835928+5934037,
@@ -199,23 +203,8 @@ ggplot()+
   theme_classic()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold"),
         strip.background=element_blank(), strip.text=element_text(face="bold", size=rel(1)))+
-  labs(title="Breaking the cycle",
-       subtitle="COVID-19 hospital admissions and deaths across England <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average</span>.<br>Admissions data is published for NHS regions while deaths data is at government region level. These geographies are similar but may not overlap perfectly.",
+  labs(title="Going round in circles",
+       subtitle="New hospital admissions with a positive COVID-19 test and deaths within 28 days of a positive test in England England <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average</span>.<br>Admissions data is published for NHS regions while deaths data is at government region level. These geographies are similar but may not overlap perfectly.",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
-dev.off()
-
-tiff("Outputs/COVIDCycleRegv2.tiff", units="in", width=10, height=7, res=500)
-ggplot(weekdata.reg)+
-  geom_path(aes(x=admrate, y=mortrate, colour=name),
-            arrow = arrow(type = "closed", angle = 30, length = unit(0.1, "inches")))+
-  scale_x_continuous(name="Daily COVID-19 admissions per 100,000")+
-  scale_y_continuous(name="Daily COVID-19 deaths per 100,000")+
-  scale_colour_paletteer_d("ggsci::default_locuszoom", name="")+
-  theme_classic()+
-  theme(plot.title=element_text(face="bold"),
-        strip.background=element_blank(), strip.text=element_text(face="bold", size=rel(1)))+
-  labs(title="Breaking the cycle",
-       subtitle="COVID-19 hospital admission and death rates across England by region\nAdmissions data is published for NHS regions while deaths data is at government region level.\nThese geographies are similar but may not overlap perfectly.",
-       caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 dev.off()
