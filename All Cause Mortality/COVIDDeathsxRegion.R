360c7b0b57401f309e092c5b8350c4ff7c2cc693
Colin Angus <c.r.angus@sheffield.ac.uk>
2021-01-18 15:11:17 +0000

    Update COVIDDeathsxRegion.R

--- All Cause Mortality/COVIDDeathsxRegion.R
+++ All Cause Mortality/COVIDDeathsxRegion.R
@@ -46,6 +46,22 @@ ggplot()+
   theme(plot.title=element_text(face="bold", size=rel(1.2)))
 dev.off()
 
+tiff("Outputs/COVIDDeathsxRegionLineRecent.tiff", units="in", width=10, height=7, res=500)
+deaths.reg %>% 
+  filter(date<maxdate-days(3) & date>as.Date("2020-09-01")) %>% 
+  ggplot()+
+  geom_line(aes(x=date, y=mortrateroll, colour=name))+
+  theme_classic()+
+  scale_x_date(name="", breaks=breaks_pretty(n=interval(min(deaths.reg$date, na.rm=TRUE), maxdate-days(3))%/% months(1)))+
+  scale_y_continuous(name="Daily COVID-19 deaths per 100,000")+
+  scale_colour_paletteer_d("LaCroixColoR::paired", name="Region")+
+  labs(title="Deaths associated with COVID-19 look like they might be beginning to fall",
+       subtitle="Daily deaths per 100,000 within 28 days of a positive COVID-19 test",
+       caption="Data from PHE | Plot by @VictimOfMaths")+
+  theme(plot.title=element_text(face="bold", size=rel(1.2)))
+dev.off()
+
+
 #Pull out peak Spring and Autumn deaths for each region
 peaks <- deaths.reg %>% 
   mutate(period=if_else(date<as.Date("2020-08-01"), "Wave 1", "Wave 2")) %>% 
@@ -67,3 +83,53 @@ ggplot(peaks)+
   theme(plot.title=element_text(face="bold", size=rel(1.2)))
 dev.off()
 
+#National version
+#Get daily regional death data
+deaths.nat <- get_data(filters="areaType=nation", structure=list(date="date",
+                                                                 name="areaName",
+                                                                 deaths="newDeaths28DaysByDeathDate"))
+
+#Bring in regional population
+deaths.nat <- deaths.nat %>% 
+  mutate(pop=case_when(
+    name=="England" ~ 56286961,
+    name=="Scotland" ~ 5463300,
+    name=="Wales" ~ 3152879,
+    name=="Northern Ireland" ~ 1893667),
+    mortrate=deaths*100000/pop,
+    date=as.Date(date)) %>% 
+  arrange(date) %>% 
+  group_by(name) %>% 
+  mutate(mortrateroll=roll_mean(mortrate, 7, align="center", fill=NA)) %>% 
+  ungroup()
+  
+
+tiff("Outputs/COVIDDeathsxCountryLine.tiff", units="in", width=10, height=7, res=500)
+deaths.nat %>% 
+  filter(date<maxdate-days(3)) %>% 
+  ggplot()+
+  geom_line(aes(x=date, y=mortrateroll, colour=name))+
+  theme_classic()+
+  scale_x_date(name="", breaks=breaks_pretty(n=interval(min(deaths.reg$date, na.rm=TRUE), maxdate-days(3))%/% months(1)))+
+  scale_y_continuous(name="Daily COVID-19 deaths per 100,000")+
+  scale_colour_paletteer_d("fishualize::Scarus_quoyi", name="")+
+  labs(title="Deaths linked to COVID-19 are falling, except in Northern Ireland",
+       subtitle="Daily deaths per 100,000 within 28 days of a positive COVID-19 test",
+       caption="Data from PHE | Plot by @VictimOfMaths")+
+  theme(plot.title=element_text(face="bold", size=rel(1.2)))
+dev.off()
+
+tiff("Outputs/COVIDDeathsxCountryLineRecent.tiff", units="in", width=10, height=7, res=500)
+deaths.nat %>% 
+  filter(date<maxdate-days(3) & date>as.Date("2020-09-01")) %>% 
+  ggplot()+
+  geom_line(aes(x=date, y=mortrateroll, colour=name))+
+  theme_classic()+
+  scale_x_date(name="", breaks=breaks_pretty(n=interval(min(deaths.reg$date, na.rm=TRUE), maxdate-days(3))%/% months(1)))+
+  scale_y_continuous(name="Daily COVID-19 deaths per 100,000")+
+  scale_colour_paletteer_d("fishualize::Scarus_quoyi", name="")+
+  labs(title="Mortality rates have taken very different trajectories in the UK nations since October",
+       subtitle="Daily deaths per 100,000 within 28 days of a positive COVID-19 test",
+       caption="Data from PHE | Plot by @VictimOfMaths")+
+  theme(plot.title=element_text(face="bold", size=rel(1.2)))
+dev.off()
