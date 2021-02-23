7f41ef005f6d2484929bbc330811e90969253b06
Colin Angus <c.r.angus@sheffield.ac.uk>
2021-02-23 15:49:58 +0000

    Update COVIDDeathsxRegion.R

--- All Cause Mortality/COVIDDeathsxRegion.R
+++ All Cause Mortality/COVIDDeathsxRegion.R
@@ -40,7 +40,7 @@ ggplot()+
   scale_x_date(name="", breaks=breaks_pretty(n=interval(min(deaths.reg$date, na.rm=TRUE), maxdate-days(3))%/% months(1)))+
   scale_y_continuous(name="Daily COVID-19 deaths per 100,000")+
   scale_colour_paletteer_d("LaCroixColoR::paired", name="Region")+
-  labs(title="Mortality trajectories in the 'second wave' have been much more varied between English regions",
+  labs(title="COVID-19 deaths are falling consistently across all regions of England",
        subtitle="Daily deaths per 100,000 within 28 days of a positive COVID-19 test",
        caption="Data from PHE | Plot by @VictimOfMaths")+
   theme(plot.title=element_text(face="bold", size=rel(1.2)))
@@ -55,7 +55,7 @@ deaths.reg %>%
   scale_x_date(name="", breaks=breaks_pretty(n=interval(min(deaths.reg$date, na.rm=TRUE), maxdate-days(3))%/% months(1)))+
   scale_y_continuous(name="Daily COVID-19 deaths per 100,000")+
   scale_colour_paletteer_d("LaCroixColoR::paired", name="Region")+
-  labs(title="Deaths associated with COVID-19 look like they might be beginning to fall",
+  labs(title="Deaths associated with COVID-19 are falling rapidly across England",
        subtitle="Daily deaths per 100,000 within 28 days of a positive COVID-19 test",
        caption="Data from PHE | Plot by @VictimOfMaths")+
   theme(plot.title=element_text(face="bold", size=rel(1.2)))
@@ -75,9 +75,9 @@ ggplot(peaks)+
   geom_abline(intercept=0)+
   scale_colour_paletteer_d("LaCroixColoR::paired", name="Region")+
   theme_classic()+
-  scale_x_continuous(limits=c(0,2.5), name="Peak mortality rate Mar-Jul\n7-day rolling average")+
-  scale_y_continuous(limits=c(0,2.5), name="Peak mortality rate Aug-Jan\n7-day rolling average")+
-  labs(title="Correlation between the Spring and Autumn/Winter peaks has strengthened",
+  scale_x_continuous(limits=c(0,3), name="Peak mortality rate Mar-Jul\n7-day rolling average")+
+  scale_y_continuous(limits=c(0,3), name="Peak mortality rate Aug-Jan\n7-day rolling average")+
+  labs(title="Correlation between the Spring and Autumn/Winter peaks is limited",
        subtitle="Deaths per 100,000 within 28 days of a positive COVID-19 test",
        caption="Date from PHE | Plot by @VictimOfMaths")+
   theme(plot.title=element_text(face="bold", size=rel(1.2)))
@@ -113,7 +113,7 @@ deaths.nat %>%
   scale_x_date(name="", breaks=breaks_pretty(n=interval(min(deaths.reg$date, na.rm=TRUE), maxdate-days(3))%/% months(1)))+
   scale_y_continuous(name="Daily COVID-19 deaths per 100,000")+
   scale_colour_paletteer_d("fishualize::Scarus_quoyi", name="")+
-  labs(title="Deaths linked to COVID-19 are falling, except in Northern Ireland",
+  labs(title="Deaths linked to COVID-19 are falling in all four UK nations",
        subtitle="Daily deaths per 100,000 within 28 days of a positive COVID-19 test",
        caption="Data from PHE | Plot by @VictimOfMaths")+
   theme(plot.title=element_text(face="bold", size=rel(1.2)))
@@ -128,7 +128,7 @@ deaths.nat %>%
   scale_x_date(name="", breaks=breaks_pretty(n=interval(min(deaths.reg$date, na.rm=TRUE), maxdate-days(3))%/% months(1)))+
   scale_y_continuous(name="Daily COVID-19 deaths per 100,000")+
   scale_colour_paletteer_d("fishualize::Scarus_quoyi", name="")+
-  labs(title="Mortality rates have taken very different trajectories in the UK nations since October",
+  labs(title="Mortality rates across the UK have been falling consistently since mid-January",
        subtitle="Daily deaths per 100,000 within 28 days of a positive COVID-19 test",
        caption="Data from PHE | Plot by @VictimOfMaths")+
   theme(plot.title=element_text(face="bold", size=rel(1.2)))
