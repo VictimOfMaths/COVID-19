d59e0002cfcc3fe09e5ca9d22fe299ab57f8d3f9
Colin Angus <c.r.angus@sheffield.ac.uk>
2021-02-25 17:26:10 +0000

    Update COVIDVaccinations.R

--- Heatmaps/COVIDVaccinations.R
+++ Heatmaps/COVIDVaccinations.R
@@ -1,162 +1,95 @@
 rm(list=ls())
 
 library(tidyverse)
-library(readxl)
+library(curl)
+library(ukcovid19)
 library(paletteer)
-library(sf)
-library(scales)
+library(ggtext)
+library(RcppRoll)
 library(ragg)
-library(gtools)
 
-#Download vaccination data by MSOA
-vax <- tempfile()
-url <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2021/02/COVID-19-weekly-announced-vaccinations-25-February-2021.xlsx"
-vax <- curl_download(url=url, destfile=vax, quiet=FALSE, mode="wb")
-
-vaxdata <- read_excel(vax, sheet="Vaccinations by MSOA", range="F16:K6806", col_names=FALSE) %>% 
-  rename(msoa11cd=`...1`, msoa11nm=`...2`, `<70`=`...3`, `70-74`=`...4`, `75-79`=`...5`, `80+`=`...6`) %>% 
-  gather(age, vaccinated, c(3:6))
-
-#Download IMD data
+#Bring in daily data
 temp <- tempfile()
-source <- ("https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/833970/File_1_-_IMD2019_Index_of_Multiple_Deprivation.xlsx")
+source="https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&metric=cumPeopleVaccinatedFirstDoseByPublishDate&metric=cumPeopleVaccinatedSecondDoseByPublishDate&metric=newPeopleVaccinatedFirstDoseByPublishDate&metric=newPeopleVaccinatedSecondDoseByPublishDate&format=csv"
 temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
+APIdata2 <- read.csv(temp)
+colnames(APIdata2) <- c("date", "type", "code", "name", "cumdose1", "cumdose2", "dose1", "dose2")
 
-IMD <- read_excel(temp, sheet="IMD2019", range="A2:F32845", col_names=FALSE)[,c(1,2,5,6)]
-colnames(IMD) <- c("LSOA11CD", "LSOA11NM", "IMDrank", "IMDdecile")
-
-#Download LSOA to MSOA lookup
-temp <- tempfile()
-source <- ("https://opendata.arcgis.com/datasets/fe6c55f0924b4734adf1cf7104a0173e_0.csv")
-temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
-
-lookup <- read.csv(temp) %>% 
-  select(LSOA11CD, MSOA11CD, RGN11NM) %>% 
-  unique()
-
-#Merge into IMD data
-IMD <- merge(IMD, lookup, by="LSOA11CD")
-
-
-#Bring in population data for LSOAs
-temp <- tempfile()
-temp2 <- tempfile()
-source <- "https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fpopulationandmigration%2fpopulationestimates%2fdatasets%2flowersuperoutputareamidyearpopulationestimatesnationalstatistics%2fmid2019sape22dt13/sape22dt13mid2019lsoabroadagesestimatesunformatted.zip"
-temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
-unzip(zipfile=temp, exdir=temp2)
-
-pop <- read_excel(file.path(temp2, "SAPE22DT13-mid-2019-lsoa-Broad_ages-estimates-unformatted.xlsx"),
-                  sheet="Mid-2019 Persons", range="A6:G34758", col_names=FALSE)[,c(1,7)]
-colnames(pop) <- c("LSOA11CD", "pop")
-
-#Merge into IMD data
-IMD <- merge(IMD, pop)
-
-#Calculate IMD rank at MSOA level as weighted average of LSOA level ranks, weight by population
-IMD_MSOA <- IMD %>% 
-  group_by(MSOA11CD) %>% 
-  summarise(IMDrank=weighted.mean(IMDrank, pop), pop=sum(pop)) %>% 
-  ungroup() 
-
-#Bring in population data for MSOAs by age
-temp <- tempfile()
-temp2 <- tempfile()
-source <- "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/populationandmigration/populationestimates/datasets/middlesuperoutputareamidyearpopulationestimates/mid2019sape22dt4/sape22dt4mid2019msoasyoaestimatesunformatted.zip"
-temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
-unzip(zipfile=temp, exdir=temp2)
-
-pop <- read_excel(file.path(temp2, "SAPE22DT4-mid-2019-msoa-syoa-estimates-unformatted.xlsx"),
-                  sheet="Mid-2019 Persons", range="A6:CT6796", col_names=FALSE) %>% 
-  select(c(1, 2, 7:98)) %>% 
-  rename(msoa11cd=`...1`, msoa11nm=`...2`) %>% 
-  rowwise() %>% 
-  mutate(`80+`=sum(c(`...88`,`...89`, `...90`, `...91`, `...92`, `...93`, `...94`, `...95`, `...96`,
-                     `...97`, `...98`)),
-         `75-79`=sum(c(`...83`, `...84`, `...85`, `...86`, `...87`)),
-         `70-74`=sum(c(`...78`, `...79`, `...80`, `...81`, `...82`)),
-         `<70`=`...7`-`70-74`-`75-79`-`80+`) %>% 
+#Generate totals
+APIdata2 <- APIdata2 %>% 
+  group_by(date) %>% 
+  summarise(cumdose1=sum(cumdose1), cumdose2=sum(cumdose2),
+            dose1=sum(dose1), dose2=sum(dose2)) %>% 
+  mutate(name="UK") %>% 
+  bind_rows(APIdata2) %>% 
+  mutate(pop=case_when(name=="England" ~ 56286961,
+                       name=="Wales" ~ 3152879,
+                       name=="Scotland" ~ 5463300,
+                       name=="Northern Ireland" ~ 1893667,
+                       name=="UK" ~ 56286961+3152879+546330+1893667),
+         dose1rate=dose1*1000000/pop,
+         dose2rate=dose2*1000000/pop,
+         cumpropdose1=cumdose1/pop,
+         cumpropdose2=cumdose2/pop,
+         date=as.Date(date)) %>% 
   ungroup() %>% 
-  select(msoa11cd, msoa11nm, `<70`, `70-74`, `75-79`, `80+`) %>% 
-  gather(age, pop, c(3:6))
-
-#COMBINE
-vaxdata <- merge(vaxdata, pop %>% select(-msoa11nm)) %>% 
-  merge(IMD_MSOA %>% select(-pop), by.x="msoa11cd", by.y="MSOA11CD") %>% 
-  mutate(vaxprop=vaccinated/pop)
-
-#Download Carl Baker's lovely cartogram
-msoa <- tempfile()
-source <- ("https://github.com/houseofcommonslibrary/uk-hex-cartograms-noncontiguous/raw/main/geopackages/MSOA.gpkg")
-msoa <- curl_download(url=source, destfile=msoa, quiet=FALSE, mode="wb")
-
-BackgroundMSOA <- st_read(msoa, layer="5 Background")
-
-MSOA <- st_read(msoa, layer="4 MSOA hex") %>% 
-  left_join(vaxdata, by="msoa11cd")
-
-GroupsMSOA <- st_read(msoa, layer="2 Groups")
-
-Group_labelsMSOA <- st_read(msoa, layer="1 Group labels") %>% 
-  mutate(just=if_else(LabelPosit=="Left", 0, 1))
-
-LAsMSOA <- st_read(msoa, layer="3 Local authority outlines (2019)")
-
-plot <- ggplot()+
-  geom_sf(data=BackgroundMSOA, aes(geometry=geom))+
-  geom_sf(data=MSOA %>% filter(age=="80+"), aes(geometry=geom, fill=vaxprop), colour=NA)+
-  geom_sf(data=LAsMSOA, aes(geometry=geom), fill=NA, colour="White", size=0.1)+
-  geom_sf(data=GroupsMSOA, aes(geometry=geom), fill=NA, colour="Black")+
-  geom_sf_text(data=Group_labelsMSOA, aes(geometry=geom, label=Group.labe,
-                                          hjust=just), size=rel(2.4), colour="Black")+
-  scale_fill_paletteer_c("pals::ocean.haline", direction=-1, 
-                         name="Proportion of\npopulation\nvaccinated", limits=c(0,NA),
-                         labels=label_percent(accuracy=1))+
-  theme_void()+
-  theme(plot.title=element_text(face="bold", size=rel(1.2)))+
-  labs(title="Vaccination rates in the over 80s",
-       subtitle="People vaccinated in England by Middle Super Output Area.\nPopulation data comes from 2019 estimates and may not match current populations.",       
-       caption="Data from NHS England and ONS, Cartogram from @carlbaker/House of Commons Library\nPlot by @VictimOfMaths")
-
-agg_tiff("Outputs/COVIDVaxMSOACartogram.tiff", units="in", width=10, height=8, res=800)
-plot
-dev.off()
-
-agg_png("Outputs/COVIDVaxMSOACartogram.png", units="in", width=10, height=8, res=800)
-plot
-dev.off()
-
-#Calculate deprivation gradients within IMD deciles
-#Allocate to deciles
-vaxdeciles <- vaxdata %>% 
-  mutate(decile=quantcut(-IMDrank, 10, labels=FALSE)) %>% 
-  group_by(age, decile) %>% 
-  mutate(decilemean=sum(vaccinated)/sum(pop)) %>% 
-  ungroup() %>% 
-  group_by(age) %>% 
-  mutate(popmean=sum(vaccinated)/sum(pop)) %>% 
+  group_by(name) %>% 
+  arrange(name, date) %>% 
+  mutate(dose1rateroll=roll_mean(dose1rate, 7, align="center", fill=NA)) %>% 
   ungroup()
 
-agg_tiff("Outputs/COVIDVaxMSOASxIMDScatter.tiff", units="in", width=12, height=8, res=800)
-ggplot(vaxdeciles %>% filter(age=="80+"), 
-       aes(x=vaxprop, y=as.factor(decile), colour=vaxprop))+
-  geom_jitter(shape=21, alpha=0.6, show.legend=FALSE)+
-  geom_segment(aes(x=popmean, xend=popmean, y=Inf, yend=-Inf), colour="Grey20")+
-  geom_point(aes(x=decilemean, y=as.factor(decile)), colour="Grey20", fill="Cyan", shape=23, size=2)+
-  scale_colour_paletteer_c("viridis::magma", direction=-1)+
-  scale_x_continuous(name="Proportion of 80+ population vaccinated",
-                     labels=label_percent(accuracy=1))+
-  scale_y_discrete(name="Index of Multiple Deprivation", labels=c("1 - most deprived", "2", "3", "4", "5", "6", "7", 
-                                                                    "8", "9", "10 - least deprived"))+  
+agg_tiff("Outputs/COVIDVaccinationRateDaily.tiff", units="in", width=8, height=6, res=500)
+APIdata2 %>% 
+  filter(name!="UK") %>% 
+ggplot()+
+  geom_line(aes(x=date, y=dose1rateroll, colour=name))+
+  scale_x_date(name="Week ending")+
+  scale_y_continuous(name="First doses delivered per 1m population", limits=c(0,NA))+
+  scale_colour_paletteer_d("fishualize::Scarus_quoyi", name="")+
   theme_classic()+
-  theme(plot.title=element_text(face="bold", size=rel(1.4)))+
-  labs(title="COVID vaccine uptake is lower in more deprived areas in England",
-       subtitle="Number of people aged 80+ vaccinated by MSOA compared compared to the estimated 80+ population in 2019.",
-       caption="Vaccination data from NHS England, Population data from ONS\nPlot by @VictimOfMaths")+
-  annotate("text", x=0.3, y=9.9, label="Each circle = 1 MSOA", size=3)+
-  annotate("text", x=0.8, y=6.5, label="Population average", size=3)+
-  annotate("text", x=1.05, y=3.5, label="Decile average", size=3)+
-  geom_segment(aes(x=0.88, y=6.5,  xend=0.942, yend=6.5), colour="Grey20")+
-  geom_segment(aes(x=1.05, y=3.55,  xend=0.975, yend=3.95), colour="Grey20")
+  theme(plot.title=element_text(face="bold", size=rel(1.2)))+
+  labs(title="Vaccine delivery has accelerated in recent weeks, except in England",
+       subtitle="Rolling 7-day average rates of delivery of 1st COVID-19 vaccine doses by country by publication date",
+       caption="Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
+
 dev.off()
 
+agg_tiff("Outputs/COVIDVaccinationTotalsDaily.tiff", units="in", width=9, height=6, res=500)
+APIdata2 %>% 
+  filter(name!="UK") %>% 
+  select(date, name, cumpropdose1, cumpropdose2) %>% 
+  gather(dose, rate, c(3,4)) %>% 
+  ggplot()+
+  geom_line(aes(x=date, y=rate, colour=name, linetype=dose))+
+  scale_x_date(name="")+
+  scale_y_continuous(name="Proportion of the population vaccinated", 
+                     labels=scales::label_percent(accuracy=1))+
+  scale_colour_paletteer_d("fishualize::Scarus_quoyi", name="Nation")+
+  scale_linetype_discrete(name="Dose", labels=c("1st", "2nd"))+
+  theme_classic()+
+  theme(plot.title=element_text(face="bold", size=rel(1.2)))+
+  labs(title="Scotland and Wales have picked up their delivery of COVID-19 💉",
+       subtitle="Rates of delivery of COVID-19 vaccines by country by publication date",
+       caption="Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
+dev.off()
+
+agg_tiff("Outputs/COVIDVaccinationTargetDaily.tiff", units="in", width=8, height=6, res=500)
+APIdata2 %>% 
+  filter(name=="UK") %>% 
+  select(date, name, cumdose1, cumdose2) %>% 
+  gather(dose, count, c(3,4)) %>% 
+  mutate(date=as.Date(date)) %>% 
+  ggplot()+
+  geom_line(aes(x=date, y=count/1000000, linetype=dose), colour="Darkred")+
+  geom_hline(yintercept=14.6, colour="SkyBlue")+
+  scale_x_date(name="", limits=c(as.Date("2021-01-10"), as.Date("2021-02-14")))+
+  scale_y_continuous(name="Total vaccine doses delivered (millions)")+
+  scale_linetype_discrete(name="Dose", labels=c("1st", "2nd"))+
+  theme_classic()+
+  theme(plot.title=element_text(face="bold", size=rel(1.2)), plot.subtitle=element_markdown())+
+  labs(title="Vaccine delivery in the UK has now surpassed the Mid-February target",
+       subtitle="<span style='color:DarkRed;'>Total COVID-19 vaccinations delivered</span>, by dose, in the UK compared to <span style='color:SkyBlue2;'>Boris Johnson's mid-February target</span><br>Data by publication date",
+       caption="Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
+
+dev.off()
