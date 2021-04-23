88bc728f2d9a3a756a92bf4c17ad830934fb1c6b
Colin Angus <c.r.angus@sheffield.ac.uk>
2021-04-23 09:29:27 +0100

    Update COVIDPHEVaxxAgexSex.R

--- Heatmaps/COVIDPHEVaxxAgexSex.R
+++ Heatmaps/COVIDPHEVaxxAgexSex.R
@@ -66,3 +66,60 @@ ggplot(data_long, aes(x=Value, y=Age, fill=SexMeasure))+
   annotate("text", x=-4000000, y=9, label="Men", colour="#6600cc", size=rel(6), fontface="bold")+
   annotate("text", x=4000000, y=9, label="Women", colour="#00cc99", size=rel(6), fontface="bold")
 dev.off()  
+
+#Produce version with compressed age bands
+#V1 10 year bands for >20
+agg_tiff("Outputs/COVIDVaxxAgexSexv2.tiff", units="in", width=8, height=6, res=800)
+data_long %>% 
+  mutate(Age=case_when(
+    Age %in% c("50-54", "55-59") ~ "50-59",
+    Age %in% c("60-64", "65-69") ~ "60-69",
+    Age %in% c("70-74", "75-79") ~ "70-79",
+    TRUE ~ as.character(Age))) %>% 
+  group_by(Age, SexMeasure) %>% 
+  summarise(Value=sum(Value)) %>% 
+  ungroup() %>% 
+  ggplot(aes(x=Value, y=Age, fill=SexMeasure))+
+  geom_col(position="stack", show.legend=FALSE)+
+  scale_x_continuous(breaks=c(-7500000, -5000000, -2500000, 0, 2500000, 5000000, 7500000),
+                     labels=c("7.5m", "5m", "2.5m", "0", "2.5m", "5m", "7.5m"),
+                     limits=c(-7500000, 7500000), name="")+
+  scale_y_discrete(name="Age group")+
+  scale_fill_manual(values=c("Grey80", "#7dffdd", "#00cc99", "Grey80", "#be7dff", "#6600cc"))+
+  theme_classic()+
+  theme(axis.line.y=element_blank(), text=element_text(family="Roboto"), 
+        plot.title=element_text(face="bold", size=rel(1.4)), plot.subtitle=element_markdown())+
+  labs(title="Vaccine delivery in England by age and sex",
+       subtitle="The number of people who are <span style='color:Grey60;'>unvaccinated</span>, men with <span style='color:#be7dff;'>one</span> or <span style='color:#6600cc;'>two</span> doses and women with <span style='color:#7dffdd;'>one</span> or <span style='color:#00cc99;'>two</span> doses",
+       caption="Data from PHE, population figures from NIMS\nPlot by @VictimOfMaths")+
+  annotate("text", x=-5000000, y=7.5, label="Men", colour="#6600cc", size=rel(6), fontface="bold")+
+  annotate("text", x=5000000, y=7.5, label="Women", colour="#00cc99", size=rel(6), fontface="bold")
+dev.off()  
+
+#V2 20 year bands
+agg_tiff("Outputs/COVIDVaxxAgexSexv3.tiff", units="in", width=8, height=5, res=800)
+data_long %>% 
+  mutate(Age=case_when(
+    Age %in% c("20-29", "30-39") ~ "20-39",
+    Age %in% c("40-49", "50-54", "55-59") ~ "40-59",
+    Age %in% c("60-64", "65-69", "70-74", "75-79") ~ "60-79",
+    TRUE ~ as.character(Age))) %>% 
+  group_by(Age, SexMeasure) %>% 
+  summarise(Value=sum(Value)) %>% 
+  ungroup() %>% 
+  ggplot(aes(x=Value, y=Age, fill=SexMeasure))+
+  geom_col(position="stack", show.legend=FALSE)+
+  scale_x_continuous(breaks=c(-7500000, -5000000, -2500000, 0, 2500000, 5000000, 7500000),
+                     labels=c("7.5m", "5m", "2.5m", "0", "2.5m", "5m", "7.5m"),
+                     limits=c(NA,NA), name="")+
+  scale_y_discrete(name="Age group")+
+  scale_fill_manual(values=c("Grey80", "#7dffdd", "#00cc99", "Grey80", "#be7dff", "#6600cc"))+
+  theme_classic()+
+  theme(axis.line.y=element_blank(), text=element_text(family="Roboto"), 
+        plot.title=element_text(face="bold", size=rel(1.4)), plot.subtitle=element_markdown())+
+  labs(title="Vaccine delivery in England by age and sex",
+       subtitle="The number of people who are <span style='color:Grey60;'>unvaccinated</span>, men with <span style='color:#be7dff;'>one</span> or <span style='color:#6600cc;'>two</span> doses and women with <span style='color:#7dffdd;'>one</span> or <span style='color:#00cc99;'>two</span> doses",
+       caption="Data from PHE, population figures from NIMS\nPlot by @VictimOfMaths")+
+  annotate("text", x=-5000000, y=5, label="Men", colour="#6600cc", size=rel(6), fontface="bold")+
+  annotate("text", x=5000000, y=5, label="Women", colour="#00cc99", size=rel(6), fontface="bold")
+dev.off()  
