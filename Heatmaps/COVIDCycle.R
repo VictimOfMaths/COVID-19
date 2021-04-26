471190af72159b7178915ecc34e8ca378db2b3d7
Colin Angus <c.r.angus@sheffield.ac.uk>
2021-04-26 14:55:38 +0100

    Update COVIDCycle.R

--- Heatmaps/COVIDCycle.R
+++ Heatmaps/COVIDCycle.R
@@ -59,7 +59,7 @@ ggplot()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold"),
         strip.background=element_blank(), strip.text=element_text(face="bold", size=rel(1)),
         text=element_text(family="Roboto"))+
-  labs(title="Heading in the right direction",
+  labs(title="Nearly there...",
        subtitle="New hospital admissions with positive COVID-19 test and deaths within 28 days of a positive test across the UK <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
@@ -78,7 +78,7 @@ EngCycle <- ggplot()+
   theme_classic()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold"),
         text=element_text(family="Roboto"))+
-  labs(title="Heading in the right direction",
+  labs(title="Nearly there...",
        subtitle="New hospital admissions with positive COVID-19 test and deaths within 28 days of a positive test<br>in England <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
@@ -104,7 +104,7 @@ ggplot()+
   theme_classic()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold"),
         text=element_text(family="Roboto"))+
-  labs(title="Heading in the right direction",
+  labs(title="Nearly there...",
        subtitle="New hospital admissions with positive COVID-19 test and deaths within 28 days of a positive test<br>in Wales <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
@@ -124,7 +124,7 @@ ggplot()+
   theme_classic()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold"),
         text=element_text(family="Roboto"))+
-  labs(title="Heading in the right direction",
+  labs(title="Nearly there...",
        subtitle="New hospital admissions with positive COVID-19 test and deaths within 28 days of a positive test<br>in Scotland <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
@@ -144,7 +144,7 @@ ggplot()+
   theme_classic()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold"),
         text=element_text(family="Roboto"))+
-  labs(title="Heading in the right direction",
+  labs(title="Nearly there...",
        subtitle="New hospital admissions with positive COVID-19 test and deaths within 28 days of a positive test<br>in Northern Ireland <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
@@ -210,7 +210,7 @@ ggplot()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold"),
         strip.background=element_blank(), strip.text=element_text(face="bold", size=rel(1)),
         text=element_text(family="Roboto"))+
-  labs(title="Heading in the right direction",
+  labs(title="Nearly there...",
        subtitle="New hospital admissions with a positive COVID-19 test and deaths within 28 days of a positive test in England England <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average</span>.<br>Admissions data is published for NHS regions while deaths data is at government region level. These geographies are similar but may not overlap perfectly.",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
