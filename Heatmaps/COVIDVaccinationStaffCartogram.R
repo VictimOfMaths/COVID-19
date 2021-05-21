971ac74140f2a6447d08df50922c42b5390a8d6f
Colin Angus <c.r.angus@sheffield.ac.uk>
2021-05-21 11:01:07 +0100

    Update COVIDVaccinationStaffCartogram.R

--- Heatmaps/COVIDVaccinationStaffCartogram.R
+++ Heatmaps/COVIDVaccinationStaffCartogram.R
@@ -13,20 +13,31 @@ library(extrafont)
 #Download vaccination data for social care staff by UTLA
 #https://www.england.nhs.uk/statistics/statistical-work-areas/covid-19-vaccinations/
 vax <- tempfile()
-url <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2021/03/COVID-19-weekly-announced-vaccinations-25-March-2021.xlsx"
+url <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2021/05/COVID-19-weekly-announced-vaccinations-20-May-2021.xlsx"
 vax <- curl_download(url=url, destfile=vax, quiet=FALSE, mode="wb")
 
-carehome <- read_excel(vax, sheet="Older Adult Care Homes by UTLA", range="B17:L166", col_names=FALSE) %>% 
-  rename(cua.name=`...1`, CHres=`...6`, CHstaff=`...11`) %>% 
-  select(cua.name, CHres, CHstaff)
+carehome <- read_excel(vax, sheet="Older Adult Care Homes by UTLA", range="B21:P170", col_names=FALSE) %>% 
+  rename(cua.name=`...1`, CHresdose1=`...6`, CHresdose2=`...8`, CHstaffdose1=`...13`,
+         CHstaffdose2=`...15`) %>% 
+  select(cua.name, CHresdose1, CHresdose2, CHstaffdose1, CHstaffdose2) %>% 
+  mutate(CHresdose1=if_else(CHresdose1=="-",0,as.numeric(CHresdose1)),
+         CHresdose2=if_else(CHresdose2=="-",0,as.numeric(CHresdose2)),
+         CHstaffdose1=if_else(CHstaffdose1=="-",0,as.numeric(CHstaffdose1)),
+         CHstaffdose2=if_else(CHstaffdose2=="-",0,as.numeric(CHstaffdose2)))
   
-soccare <- read_excel(vax, sheet="Social Care Staff by UTLA", range="B17:N167", col_names=FALSE) %>% 
-  rename(cua.name=`...1`, YAstaff=`...5`, Domstaff=`...9`, Othstaff=`...13`) %>% 
-  select(cua.name, YAstaff, Domstaff, Othstaff) %>% 
-  mutate(YAstaff=if_else(YAstaff=="	-", 0, as.numeric(YAstaff)),
-         Othstaff=if_else(Othstaff=="	-", 0, as.numeric(Othstaff)))
+soccare <- read_excel(vax, sheet="Social Care Staff by UTLA", range="B22:T170", col_names=FALSE) %>% 
+  rename(cua.name=`...1`, YAstaffdose1=`...5`, YAstaffdose2=`...7`, Domstaffdose1=`...11`, 
+         Domstaffdose2=`...13`, Othstaffdose1=`...17`, Othstaffdose2=`...19`) %>% 
+  select(cua.name, YAstaffdose1, YAstaffdose2, Domstaffdose1, Domstaffdose2,
+         Othstaffdose1, Othstaffdose2) %>% 
+  mutate(YAstaffdose1=if_else(YAstaffdose1 %in% c("-", "tbc"), 0, as.numeric(YAstaffdose1)),
+         YAstaffdose2=if_else(YAstaffdose2 %in% c("-", "tbc"), 0, as.numeric(YAstaffdose2)),
+         Domstaffdose1=if_else(Domstaffdose1 %in% c("-", "tbc"), 0, as.numeric(Domstaffdose1)),
+         Domstaffdose1=if_else(Domstaffdose2 %in% c("-", "tbc"), 0, as.numeric(Domstaffdose1)),
+         Othstaffdose1=if_else(Othstaffdose1 %in% c("-", "tbc"), 0, as.numeric(Othstaffdose1)),
+         Othstaffdose2=if_else(Othstaffdose2 %in% c("-", "tbc"), 0, as.numeric(Othstaffdose2)))
 
-data <- merge(carehome, soccare) %>% 
+data <- merge(carehome, soccare, all=TRUE) %>% 
   mutate(cua.name=case_when(
     cua.name=="Bournemouth, Christchurch and Poole" ~ "Bournemouth, Christchurch & Poole",
     TRUE ~ cua.name))
@@ -36,7 +47,7 @@ utla <- tempfile()
 source <- ("https://github.com/houseofcommonslibrary/uk-hex-cartograms-noncontiguous/raw/main/geopackages/LocalAuthorities-uppertier.gpkg")
 utla <- curl_download(url=source, destfile=utla, quiet=FALSE, mode="wb")
 
-BackgroundUTLA <- st_read(utla, layer="6 Background")
+BackgroundUTLA <- st_read(utla, layer="7 Background")
 
 UTLA <- st_read(utla, layer="5 UTLA-2020") %>% 
   left_join(data, by="cua.name", all=TRUE) %>% 
@@ -50,7 +61,7 @@ Group_labelsUTLA <- st_read(utla, layer="1 Group labels") %>%
 plot <- ggplot()+
   geom_sf(data=BackgroundUTLA %>% filter(id==45), aes(geometry=geom))+
   geom_sf(data=UTLA, 
-          aes(geometry=geom, fill=CHres), colour=NA)+
+          aes(geometry=geom, fill=CHresdose1), colour=NA)+
   geom_sf(data=GroupsUTLA %>% filter(!RegionNati %in% c("Wales", "Scotland", "Northern Ireland")), 
           aes(geometry=geom), fill=NA, colour="Black")+
   geom_sf_text(data=Group_labelsUTLA %>% filter(!RegionNati %in% c("Wales", "Scotland", "Northern Ireland")), 
@@ -62,11 +73,11 @@ plot <- ggplot()+
                          labels=label_percent(accuracy=1))+
   theme_void()+
   theme(plot.title=element_text(face="bold", size=rel(2)),
-        text=element_text(family="Roboto"), legend.position="top")+
+        text=element_text(family="Lato"), legend.position="top")+
   guides(fill = guide_colorbar(title.position = 'top', title.hjust = .5,
                                barwidth = unit(20, 'lines'), barheight = unit(.5, 'lines')))+
   labs(title="Vaccination rates in care home residents\n ",
-       caption="Data from NHS England, Cartogram from @carlbaker/House of Commons Library\nPlot by @VictimOfMaths")
+       caption="Grey areas reflect Local Authorities with missing data\nData from NHS England, Cartogram from @carlbaker/House of Commons Library\nPlot by @VictimOfMaths")
 
 agg_tiff("Outputs/COVIDVaxCHResCartogram.tiff", units="in", width=9, height=10, res=500)
 plot
@@ -75,7 +86,7 @@ dev.off()
 plot2 <- ggplot()+
   geom_sf(data=BackgroundUTLA %>% filter(id==45), aes(geometry=geom))+
   geom_sf(data=UTLA, 
-          aes(geometry=geom, fill=CHstaff), colour=NA)+
+          aes(geometry=geom, fill=CHstaffdose1), colour=NA)+
   geom_sf(data=GroupsUTLA %>% filter(!RegionNati %in% c("Wales", "Scotland", "Northern Ireland")), 
           aes(geometry=geom), fill=NA, colour="Black")+
   geom_sf_text(data=Group_labelsUTLA %>% filter(!RegionNati %in% c("Wales", "Scotland", "Northern Ireland")), 
@@ -87,11 +98,11 @@ plot2 <- ggplot()+
                          labels=label_percent(accuracy=1))+
   theme_void()+
   theme(plot.title=element_text(face="bold", size=rel(2)),
-        text=element_text(family="Roboto"), legend.position="top")+
+        text=element_text(family="Lato"), legend.position="top")+
   guides(fill = guide_colorbar(title.position = 'top', title.hjust = .5,
                                barwidth = unit(20, 'lines'), barheight = unit(.5, 'lines')))+
   labs(title="Vaccination rates in care home staff\n ",
-       caption="Data from NHS England, Cartogram from @carlbaker/House of Commons Library\nPlot by @VictimOfMaths")
+       caption="Grey areas reflect Local Authorities with missing data\nData from NHS England, Cartogram from @carlbaker/House of Commons Library\nPlot by @VictimOfMaths")
 
 agg_tiff("Outputs/COVIDVaxCHStaffCartogram.tiff", units="in", width=9, height=10, res=800)
 plot2
@@ -100,7 +111,7 @@ dev.off()
 plot3 <- ggplot()+
   geom_sf(data=BackgroundUTLA %>% filter(id==45), aes(geometry=geom))+
   geom_sf(data=UTLA, 
-          aes(geometry=geom, fill=YAstaff), colour=NA)+
+          aes(geometry=geom, fill=YAstaffdose1), colour=NA)+
   geom_sf(data=GroupsUTLA %>% filter(!RegionNati %in% c("Wales", "Scotland", "Northern Ireland")), 
           aes(geometry=geom), fill=NA, colour="Black")+
   geom_sf_text(data=Group_labelsUTLA %>% filter(!RegionNati %in% c("Wales", "Scotland", "Northern Ireland")), 
@@ -112,11 +123,11 @@ plot3 <- ggplot()+
                          labels=label_percent(accuracy=1))+
   theme_void()+
   theme(plot.title=element_text(face="bold", size=rel(2)),
-        text=element_text(family="Roboto"), legend.position="top")+
+        text=element_text(family="Lato"), legend.position="top")+
   guides(fill = guide_colorbar(title.position = 'top', title.hjust = .5,
                                barwidth = unit(20, 'lines'), barheight = unit(.5, 'lines')))+
   labs(title="Vaccination rates in staff from younger adult care homes\n ",
-       caption="Data from NHS England, Cartogram from @carlbaker/House of Commons Library\nPlot by @VictimOfMaths")
+       caption="Grey areas reflect Local Authorities with missing data\nData from NHS England, Cartogram from @carlbaker/House of Commons Library\nPlot by @VictimOfMaths")
 
 agg_tiff("Outputs/COVIDVaxYAStaffCartogram.tiff", units="in", width=9, height=10, res=800)
 plot3
@@ -125,7 +136,7 @@ dev.off()
 plot4 <- ggplot()+
   geom_sf(data=BackgroundUTLA %>% filter(id==45), aes(geometry=geom))+
   geom_sf(data=UTLA, 
-          aes(geometry=geom, fill=Domstaff), colour=NA)+
+          aes(geometry=geom, fill=Domstaffdose1), colour=NA)+
   geom_sf(data=GroupsUTLA %>% filter(!RegionNati %in% c("Wales", "Scotland", "Northern Ireland")), 
           aes(geometry=geom), fill=NA, colour="Black")+
   geom_sf_text(data=Group_labelsUTLA %>% filter(!RegionNati %in% c("Wales", "Scotland", "Northern Ireland")), 
@@ -137,11 +148,11 @@ plot4 <- ggplot()+
                          labels=label_percent(accuracy=1))+
   theme_void()+
   theme(plot.title=element_text(face="bold", size=rel(2)),
-        text=element_text(family="Roboto"), legend.position="top")+
+        text=element_text(family="Lato"), legend.position="top")+
   guides(fill = guide_colorbar(title.position = 'top', title.hjust = .5,
                                barwidth = unit(20, 'lines'), barheight = unit(.5, 'lines')))+
   labs(title="Vaccination rates in domicilliary care providers\n ",
-       caption="Data from NHS England, Cartogram from @carlbaker/House of Commons Library\nPlot by @VictimOfMaths")
+       caption="Grey areas reflect Local Authorities with missing data\nData from NHS England, Cartogram from @carlbaker/House of Commons Library\nPlot by @VictimOfMaths")
 
 agg_tiff("Outputs/COVIDVaxDomStaffCartogram.tiff", units="in", width=9, height=10, res=800)
 plot4
@@ -150,7 +161,7 @@ dev.off()
 plot5 <- ggplot()+
   geom_sf(data=BackgroundUTLA %>% filter(id==45), aes(geometry=geom))+
   geom_sf(data=UTLA, 
-          aes(geometry=geom, fill=Othstaff), colour=NA)+
+          aes(geometry=geom, fill=Othstaffdose1), colour=NA)+
   geom_sf(data=GroupsUTLA %>% filter(!RegionNati %in% c("Wales", "Scotland", "Northern Ireland")), 
           aes(geometry=geom), fill=NA, colour="Black")+
   geom_sf_text(data=Group_labelsUTLA %>% filter(!RegionNati %in% c("Wales", "Scotland", "Northern Ireland")), 
@@ -162,7 +173,7 @@ plot5 <- ggplot()+
                          labels=label_percent(accuracy=1))+
   theme_void()+
   theme(plot.title=element_text(face="bold", size=rel(2)),
-        text=element_text(family="Roboto"), legend.position="top")+
+        text=element_text(family="Lato"), legend.position="top")+
   guides(fill = guide_colorbar(title.position = 'top', title.hjust = .5,
                                barwidth = unit(20, 'lines'), barheight = unit(.5, 'lines')))+
   labs(title="Vaccination rates in other social care staff\n ",
