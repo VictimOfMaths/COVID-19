3d48dceae8162294cb4f5a88af45e73235d4e0d1
Colin Angus <c.r.angus@sheffield.ac.uk>
2021-05-21 09:53:41 +0100

    Update COVIDVaccinations.R

--- Heatmaps/COVIDVaccinations.R
+++ Heatmaps/COVIDVaccinations.R
@@ -8,6 +8,7 @@ library(ggtext)
 library(RcppRoll)
 library(ragg)
 library(extrafont)
+library(scales)
 
 #Bring in daily data
 temp <- tempfile()
@@ -71,7 +72,7 @@ APIdata2 %>%
   theme_classic()+
   theme(plot.title=element_text(face="bold", size=rel(1.2)),
         text=element_text(family="Roboto"))+
-  labs(title="Wales is getting on with delivering 2nd doses of the COVID-19 💉",
+  labs(title="The four UK nations are pursuing different vaccination strategies",
        subtitle="Rates of delivery of COVID-19 vaccines by country by publication date",
        caption="Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
@@ -96,3 +97,57 @@ APIdata2 %>%
        caption="Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
 dev.off()
+
+#UK level map using dashboard data
+source="https://api.coronavirus.data.gov.uk/v2/data?areaType=ltla&metric=cumVaccinationFirstDoseUptakeByVaccinationDatePercentage&metric=cumVaccinationSecondDoseUptakeByVaccinationDatePercentage&format=csv"
+temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
+
+vaxdata <- read.csv(temp) %>% 
+  set_names(c("Lacode", "areaName", "areaType", "date", "First dose", "Second dose")) %>% 
+  gather(dose, prop, c(5,6)) %>% 
+  mutate(date=as.Date(date))
+
+maxdate=max(vaxdata$date)
+
+#Read in Carl Baker's lovely cartogram
+ltla <- tempfile()
+source <- ("https://github.com/houseofcommonslibrary/uk-hex-cartograms-noncontiguous/raw/main/geopackages/LocalAuthorities-lowertier.gpkg")
+ltla <- curl_download(url=source, destfile=ltla, quiet=FALSE, mode="wb")
+
+Background <- st_read(ltla, layer="7 Background")
+
+ltlavax <- st_read(ltla, layer="4 LTLA-2019") %>% 
+  left_join(vaxdata, by="Lacode")
+
+Groups <- st_read(ltla, layer="2 Groups")
+
+Group_labels <- st_read(ltla, layer="1 Group labels") %>% 
+  mutate(just=if_else(LabelPosit=="Left", 0, 1))
+
+plot1 <- ggplot()+
+  geom_sf(data=Background %>% filter(Name!="Ireland"), aes(geometry=geom))+
+  geom_sf(data=ltlavax %>% filter(date==maxdate), 
+          aes(geometry=geom, fill=prop/100), colour="Black", size=0.1)+
+  geom_sf(data=Groups %>% filter(!RegionNation %in% c("Wales", "Northern Ireland")), 
+          aes(geometry=geom), fill=NA, colour="Black")+
+  geom_sf_text(data=Group_labels %>% filter(RegionNation!="Wales"), 
+               aes(geometry=geom, label=Group.labe,
+                                      hjust=just), size=rel(2.4), colour="Black")+
+  scale_fill_paletteer_c("pals::ocean.haline", direction=-1, limits=c(0,1),
+                         name="Proportion of adults vaccinated", labels=label_percent(accuracy=1))+
+  facet_wrap(~dose)+
+  theme_void()+
+  theme(plot.title=element_text(face="bold", size=rel(2.5)),
+        text=element_text(family="Lato"), legend.position="top",
+        strip.text=element_text(face="bold", size=rel(1), hjust=0))+
+  guides(fill = guide_colorbar(title.position = 'top', title.hjust = .5,
+                               barwidth = unit(20, 'lines'), barheight = unit(.5, 'lines')))+
+  labs(title="London is lagging behind in the vaccine drive",
+       subtitle="",
+       caption=paste0("Data from coronavirus.data.gov.uk, Cartogram from @carlbaker/House of Commons Library\nData up to ",
+       maxdate, "\nPlot by @VictimOfMaths"))
+
+agg_tiff("Outputs/COVIDVaxLTLACartogramEngSco.tiff", units="in", width=9, height=10, res=800)
+plot1
+dev.off()
+
