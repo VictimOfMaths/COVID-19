813af9c3432b285b8caaef6b4fd5b84e146b16e2
Colin Angus <c.r.angus@sheffield.ac.uk>
2021-06-03 14:19:04 +0100

    Update COVIDPHEVaxxAgexSex.R

--- Heatmaps/COVIDPHEVaxxAgexSex.R
+++ Heatmaps/COVIDPHEVaxxAgexSex.R
@@ -12,12 +12,12 @@ library(gganimate)
 #Download data from PHE surveillance report
 #https://www.gov.uk/government/statistics/national-flu-and-covid-19-surveillance-reports
 
-url <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/989842/Weekly_Influenza_and_COVID19_report_data_W21.xlsx"
+url <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/991080/Weekly_Influenza_and_COVID19_report_data_w22.xlsx"
 
 temp <- tempfile()
 temp <- curl_download(url=url, destfile=temp, quiet=FALSE, mode="wb")
 
-data <- read_excel(temp, sheet="Figure 58&59 COVID Vac Age Sex", range="B13:N26", col_names=FALSE) %>% 
+data <- read_excel(temp, sheet="Figure 55&56 COVID Vac Age Sex", range="B13:N26", col_names=FALSE) %>% 
   select(`...1`, `...2`, `...3`, `...5`, `...6`, `...9`, `...12`)
 
 colnames(data) <- c("Age", "Male_Pop", "Male_Vax1", "Female_Pop", "Female_Vax1", "Male_Vax2",
@@ -36,8 +36,8 @@ data_long <- pivot_longer(data, cols=c(2:7), names_to=c("Sex", "Measure"), names
                           values_to="Value") %>% 
   mutate(Value=if_else(Sex=="Male", -Value, Value),
          Age=case_when(
-           Age=="Under 20 years" ~ "<20",
-           Age=="20 to 24 years" ~ "20-24",
+           Age=="Under 18 years" ~ "<18",
+           Age=="18 to 24 years" ~ "20-24",
            Age=="25 to 29 years" ~ "25-29",
            Age=="30 to 34 years" ~ "30-34",
            Age=="35 to 39 years" ~ "35-39",
@@ -50,7 +50,7 @@ data_long <- pivot_longer(data, cols=c(2:7), names_to=c("Sex", "Measure"), names
            Age=="70 to 74 years" ~ "70-74",
            Age=="75 to 79 years" ~ "75-79",
            TRUE ~ "80+"),
-         Age=factor(Age, levels=c("<20", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49",
+         Age=factor(Age, levels=c("<18", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49",
                                   "50-54", "55-59", "60-64",
                                   "65-69", "70-74", "75-79", "80+")),
          SexMeasure=paste0(Sex, Measure))
@@ -64,13 +64,15 @@ ggplot(data_long, aes(x=Value, y=Age, fill=SexMeasure))+
   scale_y_discrete(name="Age group")+
   scale_fill_manual(values=c("Grey80", "#7dffdd", "#00cc99", "Grey80", "#be7dff", "#6600cc"))+
   theme_classic()+
-  theme(axis.line.y=element_blank(), text=element_text(family="Roboto"), 
-        plot.title=element_text(face="bold", size=rel(1.4)), plot.subtitle=element_markdown())+
+  theme(axis.line.y=element_blank(), text=element_text(family="Lato"), 
+        plot.title=element_text(face="bold", size=rel(1.6)), plot.subtitle=element_markdown())+
   labs(title="Vaccine delivery in England by age and sex",
        subtitle="The number of people who are <span style='color:Grey60;'>unvaccinated</span>, men with <span style='color:#be7dff;'>one</span> or <span style='color:#6600cc;'>two</span> doses and women with <span style='color:#7dffdd;'>one</span> or <span style='color:#00cc99;'>two</span> doses",
        caption="Data from PHE, population figures from NIMS\nPlot by @VictimOfMaths")+
-  annotate("text", x=-4000000, y=9, label="Men", colour="#6600cc", size=rel(6), fontface="bold")+
-  annotate("text", x=4000000, y=9, label="Women", colour="#00cc99", size=rel(6), fontface="bold")
+  annotate("text", x=-4000000, y=9, label="Men", colour="#6600cc", size=rel(6), 
+           fontface="bold", family="Lato")+
+  annotate("text", x=4000000, y=9, label="Women", colour="#00cc99", size=rel(6), 
+           fontface="bold", family="Lato")
 dev.off()  
 
 #Produce version with compressed age bands
@@ -132,6 +134,18 @@ dev.off()
 
 #Animated versions
 #Read in historic data
+
+#Week 20
+url <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/989842/Weekly_Influenza_and_COVID19_report_data_W21.xlsx"
+
+temp <- tempfile()
+temp <- curl_download(url=url, destfile=temp, quiet=FALSE, mode="wb")
+
+dataw20 <- read_excel(temp, sheet="Figure 58&59 COVID Vac Age Sex", range="B13:N26", col_names=FALSE) %>% 
+  select(`...1`, `...2`, `...3`, `...5`, `...6`, `...9`, `...12`)
+
+colnames(dataw20) <- c("Age", "Male_Pop", "Male_Vax1", "Female_Pop", "Female_Vax1", "Male_Vax2",
+                    "Female_Vax2")
 
 #Week 19
 url <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/988026/Weekly_Influenza_and_COVID19_report_data_w20.xlsx"
@@ -202,7 +216,8 @@ dataw14 <- read_excel(temp, sheet="Figure 58&59 COVID Vac Age Sex", range="B31:N
 colnames(dataw14) <- c("Age", "Male_Pop", "Male_Vax1", "Female_Pop", "Female_Vax1", "Male_Vax2",
                        "Female_Vax2")
 
-mergeddata <- dataw19 %>% mutate(Week=19)
+mergeddata <- dataw20 %>% mutate(Week=20) %>% 
+  bind_rows(dataw19 %>% mutate(Week=19)) %>% 
   bind_rows(dataw18 %>% mutate(Week=18)) %>% 
   bind_rows(dataw17 %>% mutate(Week=17)) %>% 
   bind_rows(dataw16 %>% mutate(Week=16)) %>% 
@@ -215,7 +230,7 @@ mergeddata <- dataw19 %>% mutate(Week=19)
          Female_Vax1Only=Female_Vax1-Female_Vax2) %>% 
   ungroup() %>% 
   select(Age, Week, Male_Unvax, Female_Unvax, Male_Vax1Only, Female_Vax1Only, Male_Vax2, Female_Vax2) %>% 
-  bind_rows(data %>% mutate(Week=20))
+  bind_rows(data %>% mutate(Week=21))
 
 mergeddata_long <- pivot_longer(mergeddata, cols=c(3:8), names_to=c("Sex", "Measure"), names_sep="_", 
                           values_to="Value") %>% 
