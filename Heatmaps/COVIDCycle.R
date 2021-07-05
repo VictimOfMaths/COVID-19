d2332bac4b8bdcdbf2ec198ac890bc5f1cc55d07
Colin Angus <c.r.angus@sheffield.ac.uk>
2021-07-05 16:19:31 +0100

    Update COVIDCycle.R

--- Heatmaps/COVIDCycle.R
+++ Heatmaps/COVIDCycle.R
@@ -39,10 +39,16 @@ weekdata <- data %>%
   group_by(weekno, name) %>% 
   summarise(admissions=mean(admissions), deaths=mean(deaths),
             admrate=mean(admrate), mortrate=mean(mortrate)) %>% 
-  mutate(label=as.Date("2020-01-01")+days(weekno)*7-1, label=format(label, "%d %b")) %>% 
+  mutate(label=as.Date("2020-01-01")+days(weekno)*7-1, label=format(label, "%d %b %y")) %>% 
+  ungroup() %>% 
+  #Pull out every 4th week for labelling, making sure the most recent week is always labelled
+  #I bet there is a more elegant solution than this, but I just learned that %% does, so I am unrepentant
+  group_by(name) %>% 
+  arrange(-weekno) %>% 
+  mutate(flag=(max(weekno)-weekno) %% 4) %>% 
   ungroup() %>% 
   arrange(weekno)
-
+  
 #Faceted plot for UK nations
 tiff("Outputs/COVIDCycleUK.tiff", units="in", width=12, height=8, res=500)
 ggplot()+
@@ -50,7 +56,8 @@ ggplot()+
             arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
   geom_path(data=weekdata, aes(x=admrate, y=mortrate), colour="tomato",
             arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
-  geom_text(data=weekdata, aes(x=admrate, y=mortrate, label=label), size=rel(2), colour="Grey40",
+  geom_text(data=weekdata %>% filter(flag==0), 
+            aes(x=admrate, y=mortrate, label=label), size=rel(2), colour="Grey40",
             vjust=-0.4)+
   scale_x_continuous(trans="log10", name="Daily COVID-19 admissions per 100,000 (log scale)")+
   scale_y_continuous(trans="log10", name="Daily COVID-19 deaths per 100,000 (log scale)")+
@@ -59,7 +66,7 @@ ggplot()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold", size=rel(1.6)),
         strip.background=element_blank(), strip.text=element_text(face="bold", size=rel(1)),
         text=element_text(family="Lato"))+
-  labs(title="Nearly there...?",
+  labs(title="Going round in circles",
        subtitle="New hospital admissions with positive COVID-19 test and deaths within 28 days of a positive test across the UK <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
@@ -71,14 +78,15 @@ EngCycle <- ggplot()+
             arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
   geom_path(data=subset(weekdata, name=="England"), aes(x=admissions, y=deaths), colour="tomato",
             arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
-  geom_text(data=subset(weekdata, name=="England"), aes(x=admissions, y=deaths, label=label), size=rel(2), colour="Grey40",
+  geom_text(data=weekdata %>% filter(name=="England" & flag==0), 
+            aes(x=admissions, y=deaths, label=label), size=rel(2), colour="Grey40",
             vjust=-0.4)+
  scale_x_continuous(trans="log10", name="Daily COVID-19 admissions (log scale)")+
  scale_y_continuous(trans="log10", name="Daily COVID-19 deaths (log scale)")+
   theme_classic()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold", size=rel(2)),
         text=element_text(family="Lato"))+
-  labs(title="Nearly there...?",
+  labs(title="Going round in circles",
        subtitle="New hospital admissions with positive COVID-19 test and deaths within 28 days of a positive test<br>in England <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
@@ -97,14 +105,15 @@ ggplot()+
             arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
   geom_path(data=subset(weekdata, name=="Wales"), aes(x=admissions, y=deaths), colour="tomato",
             arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
-  geom_text(data=subset(weekdata, name=="Wales"), aes(x=admissions, y=deaths, label=label), size=rel(2), colour="Grey40",
+  geom_text(data=weekdata %>% filter(name=="Wales" & flag==0), 
+            aes(x=admissions, y=deaths, label=label), size=rel(2), colour="Grey40",
             vjust=-0.4)+
   scale_x_continuous(trans="log10", name="Daily COVID-19 admissions (log scale)")+
   scale_y_continuous(trans="log10", name="Daily COVID-19 deaths (log scale)")+
   theme_classic()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold", size=rel(2)),
         text=element_text(family="Lato"))+
-  labs(title="Nearly there...?",
+  labs(title="Going round in circles",
        subtitle="New hospital admissions with positive COVID-19 test and deaths within 28 days of a positive test<br>in Wales <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
@@ -117,14 +126,15 @@ ggplot()+
             arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
   geom_path(data=subset(weekdata, name=="Scotland"), aes(x=admissions, y=deaths), colour="tomato",
             arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
-  geom_text(data=subset(weekdata, name=="Scotland"), aes(x=admissions, y=deaths, label=label), size=rel(2), colour="Grey40",
+  geom_text(data=weekdata %>% filter(name=="Scotland" & flag==0), 
+            aes(x=admissions, y=deaths, label=label), size=rel(2), colour="Grey40",
             vjust=-0.4)+
   scale_x_continuous(trans="log10", name="Daily COVID-19 admissions (log scale)")+
   scale_y_continuous(trans="log10", name="Daily COVID-19 deaths (log scale)")+
   theme_classic()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold", size=rel(2)),
         text=element_text(family="Lato"))+
-  labs(title="Nearly there...?",
+  labs(title="Going round in circles",
        subtitle="New hospital admissions with positive COVID-19 test and deaths within 28 days of a positive test<br>in Scotland <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
@@ -137,14 +147,15 @@ ggplot()+
             arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
   geom_path(data=subset(weekdata, name=="Northern Ireland"), aes(x=admissions, y=deaths), colour="tomato",
             arrow = arrow(type = "closed", angle = 30, length = unit(0.05, "inches")))+
-  geom_text(data=subset(weekdata, name=="Northern Ireland"), aes(x=admissions, y=deaths, label=label), size=rel(2), colour="Grey40",
+  geom_text(data=weekdata %>% filter(name=="Northern Ireland" & flag==0), 
+            aes(x=admissions, y=deaths, label=label), size=rel(2), colour="Grey40",
             vjust=-0.4)+
   scale_x_continuous(trans="log10", name="Daily COVID-19 admissions (log scale)")+
   scale_y_continuous(trans="log10", name="Daily COVID-19 deaths (log scale)")+
   theme_classic()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold", size=rel(2)),
         text=element_text(family="Lato"))+
-  labs(title="Nearly there...?",
+  labs(title="Going round in circles",
        subtitle="New hospital admissions with positive COVID-19 test and deaths within 28 days of a positive test<br>in Northern Ireland <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
@@ -192,9 +203,13 @@ weekdata.reg <- data.reg %>%
             admrate=mean(admrate), mortrate=mean(mortrate)) %>% 
   mutate(label=as.Date("2020-01-01")+days(weekno)*7-1, label=format(label, "%d %b")) %>% 
   ungroup() %>% 
+  group_by(name) %>% 
+  arrange(-weekno) %>% 
+  mutate(flag=(max(weekno)-weekno) %% 4) %>% 
+  ungroup() %>% 
   arrange(weekno)
 
-#Faceted plot for UK nations
+#Faceted plot for English regions
 tiff("Outputs/COVIDCycleReg.tiff", units="in", width=14, height=8, res=500)
 ggplot()+
   geom_path(data=data.reg, aes(x=admrate, y=mortrate), alpha=0.1,
@@ -210,7 +225,7 @@ ggplot()+
   theme(plot.subtitle=element_markdown(), plot.title=element_text(face="bold", size=rel(2)),
         strip.background=element_blank(), strip.text=element_text(face="bold", size=rel(1)),
         text=element_text(family="Lato"))+
-  labs(title="Nearly there...?",
+  labs(title="Going round in circles",
        subtitle="New hospital admissions with a positive COVID-19 test and deaths within 28 days of a positive test in England England <span style='color:Grey60;'>by day</span> and <span style='color:tomato;'>the weekly average</span>.<br>Admissions data is published for NHS regions while deaths data is at government region level. These geographies are similar but may not overlap perfectly.",
        caption="Inspired by @maartenzam | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
 
