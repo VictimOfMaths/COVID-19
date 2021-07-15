9d5080aa855491c8a9bab9d0aeadce46c0e1ebe3
Colin Angus <c.r.angus@sheffield.ac.uk>
2021-07-15 15:24:34 +0100

    Update COVIDPHEVaxxAgexSex.R

--- Heatmaps/COVIDPHEVaxxAgexSex.R
+++ Heatmaps/COVIDPHEVaxxAgexSex.R
@@ -10,14 +10,15 @@ library(lubridate)
 library(gganimate)
 
 #Download data from PHE surveillance report
-#https://www.gov.uk/government/statistics/national-flu-and-covid-19-surveillance-reports
+#https://www.gov.uk/government/statistics/national-flu-and-covid-19-surveillance-reports-2021-to-2022-season
 
-url <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1000375/Weekly_Influenza_and_COVID19_report_data_w27.xlsx"
+
+url <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1002563/Weekly_Influenza_and_COVID19_report_data_w28.xlsx"
 
 temp <- tempfile()
 temp <- curl_download(url=url, destfile=temp, quiet=FALSE, mode="wb")
 
-data <- read_excel(temp, sheet="Figure 57&58. COVID Vac Age Sex", range="B13:N26", col_names=FALSE) %>% 
+data <- read_excel(temp, sheet="Figure 61&62. COVID Vac Age Sex", range="B13:N26", col_names=FALSE) %>% 
   select(`...1`, `...2`, `...3`, `...5`, `...6`, `...9`, `...12`)
 
 colnames(data) <- c("Age", "Male_Pop", "Male_Vax1", "Female_Pop", "Female_Vax1", "Male_Vax2",
@@ -75,14 +76,14 @@ ggplot(data_long, aes(x=Value, y=Age, fill=SexMeasure))+
            fontface="bold", family="Lato")
 dev.off()  
 
-#TODO fix the change in age bands a couple of weeks ago, currently everything below here
-#is a bit broken.
-
 #Produce version with compressed age bands
 #V1 10 year bands for >20
 agg_tiff("Outputs/COVIDVaxxAgexSexv2.tiff", units="in", width=8, height=6, res=800)
 data_long %>% 
   mutate(Age=case_when(
+    Age %in% c("18-24", "25-29") ~ "18-29",
+    Age %in% c("30-34", "35-39") ~ "30-39",
+    Age %in% c("40-44", "45-49") ~ "40-49",
     Age %in% c("50-54", "55-59") ~ "50-59",
     Age %in% c("60-64", "65-69") ~ "60-69",
     Age %in% c("70-74", "75-79") ~ "70-79",
@@ -98,21 +99,23 @@ data_long %>%
   scale_y_discrete(name="Age group")+
   scale_fill_manual(values=c("Grey80", "#7dffdd", "#00cc99", "Grey80", "#be7dff", "#6600cc"))+
   theme_classic()+
-  theme(axis.line.y=element_blank(), text=element_text(family="Roboto"), 
+  theme(axis.line.y=element_blank(), text=element_text(family="Lato"), 
         plot.title=element_text(face="bold", size=rel(1.4)), plot.subtitle=element_markdown())+
   labs(title="Vaccine delivery in England by age and sex",
        subtitle="The number of people who are <span style='color:Grey60;'>unvaccinated</span>, men with <span style='color:#be7dff;'>one</span> or <span style='color:#6600cc;'>two</span> doses and women with <span style='color:#7dffdd;'>one</span> or <span style='color:#00cc99;'>two</span> doses",
        caption="Data from PHE, population figures from NIMS\nPlot by @VictimOfMaths")+
-  annotate("text", x=-5000000, y=7.5, label="Men", colour="#6600cc", size=rel(6), fontface="bold")+
-  annotate("text", x=5000000, y=7.5, label="Women", colour="#00cc99", size=rel(6), fontface="bold")
+  annotate("text", x=-5000000, y=6.5, label="Men", colour="#6600cc", size=rel(6), fontface="bold",
+           family="Lato")+
+  annotate("text", x=5000000, y=6.5, label="Women", colour="#00cc99", size=rel(6), fontface="bold",
+           family="Lato")
 dev.off()  
 
 #V2 20 year bands
 agg_tiff("Outputs/COVIDVaxxAgexSexv3.tiff", units="in", width=8, height=5, res=800)
 data_long %>% 
   mutate(Age=case_when(
-    Age %in% c("20-29", "30-39") ~ "20-39",
-    Age %in% c("40-49", "50-54", "55-59") ~ "40-59",
+    Age %in% c("18-24", "25-29", "30-34", "35-39") ~ "18-39",
+    Age %in% c("40-44", "45-49", "50-54", "55-59") ~ "40-59",
     Age %in% c("60-64", "65-69", "70-74", "75-79") ~ "60-79",
     TRUE ~ as.character(Age))) %>% 
   group_by(Age, SexMeasure) %>% 
@@ -126,17 +129,31 @@ data_long %>%
   scale_y_discrete(name="Age group")+
   scale_fill_manual(values=c("Grey80", "#7dffdd", "#00cc99", "Grey80", "#be7dff", "#6600cc"))+
   theme_classic()+
-  theme(axis.line.y=element_blank(), text=element_text(family="Roboto"), 
+  theme(axis.line.y=element_blank(), text=element_text(family="Lato"), 
         plot.title=element_text(face="bold", size=rel(1.4)), plot.subtitle=element_markdown())+
   labs(title="Vaccine delivery in England by age and sex",
        subtitle="The number of people who are <span style='color:Grey60;'>unvaccinated</span>, men with <span style='color:#be7dff;'>one</span> or <span style='color:#6600cc;'>two</span> doses and women with <span style='color:#7dffdd;'>one</span> or <span style='color:#00cc99;'>two</span> doses",
        caption="Data from PHE, population figures from NIMS\nPlot by @VictimOfMaths")+
-  annotate("text", x=-5000000, y=5, label="Men", colour="#6600cc", size=rel(6), fontface="bold")+
-  annotate("text", x=5000000, y=5, label="Women", colour="#00cc99", size=rel(6), fontface="bold")
+  annotate("text", x=-5000000, y=5, label="Men", colour="#6600cc", size=rel(6), fontface="bold",
+           family="Lato")+
+  annotate("text", x=5000000, y=5, label="Women", colour="#00cc99", size=rel(6), fontface="bold",
+           family="Lato")
 dev.off()  
 
 #Animated versions
 #Read in historic data
+#Week 26
+url <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1000375/Weekly_Influenza_and_COVID19_report_data_w27.xlsx"
+
+temp <- tempfile()
+temp <- curl_download(url=url, destfile=temp, quiet=FALSE, mode="wb")
+
+dataw26 <- read_excel(temp, sheet="Figure 57&58. COVID Vac Age Sex", range="B13:N26", col_names=FALSE) %>% 
+  select(`...1`, `...2`, `...3`, `...5`, `...6`, `...9`, `...12`)
+
+colnames(dataw26) <- c("Age", "Male_Pop", "Male_Vax1", "Female_Pop", "Female_Vax1", "Male_Vax2",
+                    "Female_Vax2")
+
 #Week 25
 
 url <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/998394/Weekly_Influenza_and_COVID19_report_data_W26.xlsx"
@@ -281,13 +298,14 @@ dataw14 <- read_excel(temp, sheet="Figure 58&59 COVID Vac Age Sex", range="B31:N
 colnames(dataw14) <- c("Age", "Male_Pop", "Male_Vax1", "Female_Pop", "Female_Vax1", "Male_Vax2",
                        "Female_Vax2")
 
-mergeddata <- dataw20 %>% mutate(Week=20) %>% 
-  bind_rows(dataw19 %>% mutate(Week=19)) %>% 
-  bind_rows(dataw18 %>% mutate(Week=18)) %>% 
-  bind_rows(dataw17 %>% mutate(Week=17)) %>% 
-  bind_rows(dataw16 %>% mutate(Week=16)) %>% 
-  bind_rows(dataw15 %>% mutate(Week=15)) %>% 
-  bind_rows(dataw14 %>% mutate(Week=14)) %>% 
+#Data for weeks prior to 21 uses different age bands. Aligning them is a project for the interested reader...
+
+mergeddata <- dataw26 %>% mutate(Week=26) %>% 
+  bind_rows(dataw25 %>% mutate(Week=25)) %>% 
+  bind_rows(dataw24 %>% mutate(Week=24)) %>% 
+  bind_rows(dataw23 %>% mutate(Week=23)) %>% 
+  bind_rows(dataw22 %>% mutate(Week=22)) %>% 
+  bind_rows(dataw21 %>% mutate(Week=21)) %>% 
   rowwise() %>% 
   mutate(Male_Unvax=Male_Pop-Male_Vax1,
          Female_Unvax=Female_Pop-Female_Vax1,
@@ -295,16 +313,19 @@ mergeddata <- dataw20 %>% mutate(Week=20) %>%
          Female_Vax1Only=Female_Vax1-Female_Vax2) %>% 
   ungroup() %>% 
   select(Age, Week, Male_Unvax, Female_Unvax, Male_Vax1Only, Female_Vax1Only, Male_Vax2, Female_Vax2) %>% 
-  bind_rows(data %>% mutate(Week=21))
+  bind_rows(data %>% mutate(Week=27))
 
 mergeddata_long <- pivot_longer(mergeddata, cols=c(3:8), names_to=c("Sex", "Measure"), names_sep="_", 
                           values_to="Value") %>% 
   mutate(Value=if_else(Sex=="Male", -Value, Value),
          Age=case_when(
-           Age=="Under 20 years" ~ "<20",
-           Age=="20 to 29 years" ~ "20-29",
-           Age=="30 to 39 years" ~ "30-39",
-           Age=="40 to 49 years" ~ "40-49",
+           Age=="Under 18 years" ~ "<18",
+           Age=="18 to 24 years" ~ "18-24",
+           Age=="25 to 29 years" ~ "25-29",
+           Age=="30 to 34 years" ~ "30-34",
+           Age=="35 to 39 years" ~ "35-39",
+           Age=="40 to 44 years" ~ "40-44",
+           Age=="45 to 49 years" ~ "45-49",
            Age=="50 to 54 years" ~ "50-54",
            Age=="55 to 59 years" ~ "55-59",
            Age=="60 to 64 years" ~ "60-64",
@@ -312,7 +333,8 @@ mergeddata_long <- pivot_longer(mergeddata, cols=c(3:8), names_to=c("Sex", "Meas
            Age=="70 to 74 years" ~ "70-74",
            Age=="75 to 79 years" ~ "75-79",
            TRUE ~ "80+"),
-         Age=factor(Age, levels=c("<20", "20-29", "30-39", "40-49", "50-54", "55-59", "60-64",
+         Age=factor(Age, levels=c("<18", "18-24", "25-29", "30-34", "35-39", "40-44", "45-49",
+                                  "50-54", "55-59", "60-64",
                                   "65-69", "70-74", "75-79", "80+")),
          SexMeasure=paste0(Sex, Measure),
          date=as.Date("2021-04-11")+weeks(Week-14))
@@ -325,11 +347,13 @@ anim <- ggplot(mergeddata_long, aes(x=Value, y=Age, fill=SexMeasure))+
   scale_y_discrete(name="Age group")+
   scale_fill_manual(values=c("Grey80", "#7dffdd", "#00cc99", "Grey80", "#be7dff", "#6600cc"))+
   theme_classic()+
-  theme(axis.line.y=element_blank(), text=element_text(family="Roboto"), 
+  theme(axis.line.y=element_blank(), text=element_text(family="Lato"), 
         plot.title=element_text(face="bold", size=rel(1.4)), plot.subtitle=element_markdown())+
   labs(caption="Data from PHE, population figures from NIMS\nPlot by @VictimOfMaths")+
-  annotate("text", x=-4000000, y=9, label="Men", colour="#6600cc", size=rel(6), fontface="bold")+
-  annotate("text", x=4000000, y=9, label="Women", colour="#00cc99", size=rel(6), fontface="bold")+
+  annotate("text", x=-4000000, y=9, label="Men", colour="#6600cc", size=rel(6), fontface="bold",
+           family="Lato")+
+  annotate("text", x=4000000, y=9, label="Women", colour="#00cc99", size=rel(6), fontface="bold",
+           family="Lato")+
   transition_states(date, transition_length=2, state_length=1, wrap=FALSE)+
   ggtitle("Vaccine delivery in England by age and sex up to {closest_state}",
           subtitle="The number of people who are <span style='color:Grey60;'>unvaccinated</span>, men with <span style='color:#be7dff;'>one</span> or <span style='color:#6600cc;'>two</span> doses<br>and women with <span style='color:#7dffdd;'>one</span> or <span style='color:#00cc99;'>two</span> doses")
