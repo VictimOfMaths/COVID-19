9d2f376cc2c4a396272f882f8adeb858573111e3
Colin Angus <c.r.angus@sheffield.ac.uk>
2021-09-27 11:07:34 +0100

    Update COVIDPHEVaxxAgexSex.R

--- Heatmaps/COVIDPHEVaxxAgexSex.R
+++ Heatmaps/COVIDPHEVaxxAgexSex.R
@@ -13,18 +13,19 @@ library(gganimate)
 #https://www.gov.uk/government/statistics/national-flu-and-covid-19-surveillance-reports-2021-to-2022-season
 
 
-url <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1008860/Weekly_Influenza_and_COVID19_report_data_W31.xlsx"
+url <- "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1020037/Weekly_Influenza_and_COVID19_report_data_W38_v2.xlsx"
 
 temp <- tempfile()
 temp <- curl_download(url=url, destfile=temp, quiet=FALSE, mode="wb")
 
-data <- read_excel(temp, sheet="Figure 62&63. COVID Vac Age Sex", range="B13:N26", col_names=FALSE) %>% 
+data <- read_excel(temp, sheet="Figure 62&63. COVID Vac Age Sex", range="B13:N30", col_names=FALSE) %>% 
   select(`...1`, `...2`, `...3`, `...5`, `...6`, `...9`, `...12`)
 
 colnames(data) <- c("Age", "Male_Pop", "Male_Vax1", "Female_Pop", "Female_Vax1", "Male_Vax2",
                      "Female_Vax2")
 
 data <- data %>% 
+  filter(Age!="18 to under 25") %>% 
   rowwise() %>% 
   mutate(Male_Unvax=Male_Pop-Male_Vax1,
          Female_Unvax=Female_Pop-Female_Vax1,
@@ -37,31 +38,34 @@ data_long <- pivot_longer(data, cols=c(2:7), names_to=c("Sex", "Measure"), names
                           values_to="Value") %>% 
   mutate(Value=if_else(Sex=="Male", -Value, Value),
          Age=case_when(
-           Age=="Under 18 years" ~ "<18",
-           Age=="18 to 24 years" ~ "18-24",
-           Age=="25 to 29 years" ~ "25-29",
-           Age=="30 to 34 years" ~ "30-34",
-           Age=="35 to 39 years" ~ "35-39",
-           Age=="40 to 44 years" ~ "40-44",
-           Age=="45 to 49 years" ~ "45-49",
-           Age=="50 to 54 years" ~ "50-54",
-           Age=="55 to 59 years" ~ "55-59",
-           Age=="60 to 64 years" ~ "60-64",
-           Age=="65 to 69 years" ~ "65-69",
-           Age=="70 to 74 years" ~ "70-74",
-           Age=="75 to 79 years" ~ "75-79",
+           Age=="Under 12" ~ "<12",
+           Age=="12 to under 16" ~ "12-15",
+           Age=="16 to under 18" ~ "16-17",
+           Age=="18 to under 20" ~ "18-19",
+           Age=="20 to under 25" ~ "20-24",
+           Age=="25 to under 30" ~ "25-29",
+           Age=="30 to under 35" ~ "30-34",
+           Age=="35 to under 40" ~ "35-39",
+           Age=="40 to under 45" ~ "40-44",
+           Age=="45 to under 50" ~ "45-49",
+           Age=="50 to under 55" ~ "50-54",
+           Age=="55 to under 60" ~ "55-59",
+           Age=="60 to under 65" ~ "60-64",
+           Age=="65 to under 70" ~ "65-69",
+           Age=="70 to under 75" ~ "70-74",
+           Age=="75 to under 80" ~ "75-79",
            TRUE ~ "80+"),
-         Age=factor(Age, levels=c("<18", "18-24", "25-29", "30-34", "35-39", "40-44", "45-49",
-                                  "50-54", "55-59", "60-64",
-                                  "65-69", "70-74", "75-79", "80+")),
+         Age=factor(Age, levels=c("<12", "12-15", "16-17", "18-19", "20-24", "25-29", "30-34", "35-39", 
+                                  "40-44", "45-49", "50-54", "55-59", "60-64", "65-69", "70-74", "75-79", 
+                                  "80+")),
          SexMeasure=paste0(Sex, Measure))
 
 agg_tiff("Outputs/COVIDVaxxAgexSex.tiff", units="in", width=8, height=7, res=800)
 ggplot(data_long, aes(x=Value, y=Age, fill=SexMeasure))+
   geom_col(position="stack", show.legend=FALSE)+
-  scale_x_continuous(breaks=c(-7500000, -5000000, -2500000, 0, 2500000, 5000000, 7500000),
-                     labels=c("7.5m", "5m", "2.5m", "0", "2.5m", "5m", "7.5m"),
-                     limits=c(-7500000, 7500000), name="")+
+  scale_x_continuous(breaks=c(-5000000, -2500000, 0, 2500000, 5000000),
+                     labels=c("5m", "2.5m", "0", "2.5m", "5m"),
+                     limits=c(-5000000, 5000000), name="")+
   scale_y_discrete(name="Age group")+
   scale_fill_manual(values=c("Grey80", "#7dffdd", "#00cc99", "Grey80", "#be7dff", "#6600cc"))+
   theme_classic()+
@@ -75,6 +79,144 @@ ggplot(data_long, aes(x=Value, y=Age, fill=SexMeasure))+
   annotate("text", x=4000000, y=9, label="Women", colour="#00cc99", size=rel(6), 
            fontface="bold", family="Lato")
 dev.off()  
+
+agg_tiff("Outputs/COVIDVaxxAgexSexv2.tiff", units="in", width=8, height=7, res=800)
+ggplot(data_long %>% 
+         mutate(SexMeasure=factor(SexMeasure, levels=c("FemaleVax2", "FemaleVax1Only", "FemaleUnvax",
+                                                       "MaleVax2", "MaleVax1Only", "MaleUnvax"))), 
+       aes(x=Value, y=Age, fill=SexMeasure))+
+  geom_col(position="stack", show.legend=FALSE)+
+  geom_segment(aes(x=0, xend=0, y=0.5, yend=17.5))+
+  scale_x_continuous(breaks=c(-5000000, -2500000, 0, 2500000, 5000000),
+                     labels=c("5m", "2.5m", "0", "2.5m", "5m"),
+                     limits=c(-5000000, 5000000), name="")+
+  scale_y_discrete(name="Age group")+
+  scale_fill_manual(values=c("#00cc99", "#7dffdd", "Grey80",  "#6600cc", "#be7dff","Grey80"))+
+  theme_classic()+
+  theme(axis.line.y=element_blank(), text=element_text(family="Lato"), 
+        plot.title=element_text(face="bold", size=rel(1.6)), plot.subtitle=element_markdown())+
+  labs(title="Vaccine delivery in England by age and sex",
+       subtitle="The number of people who are <span style='color:Grey60;'>unvaccinated</span>, men with <span style='color:#be7dff;'>one</span> or <span style='color:#6600cc;'>two</span> doses and women with <span style='color:#7dffdd;'>one</span> or <span style='color:#00cc99;'>two</span> doses",
+       caption="Data from PHE, population figures from NIMS\nPlot by @VictimOfMaths")+
+  annotate("text", x=-4000000, y=9, label="Men", colour="#6600cc", size=rel(6), 
+           fontface="bold", family="Lato")+
+  annotate("text", x=4000000, y=9, label="Women", colour="#00cc99", size=rel(6), 
+           fontface="bold", family="Lato")
+dev.off()
+
+#Bring in ONS 2020 population figures for comparison
+url <- "https://www.ons.gov.uk/file?uri=%2fpeoplepopulationandcommunity%2fpopulationandmigration%2fpopulationestimates%2fdatasets%2fpopulationestimatesforukenglandandwalesscotlandandnorthernireland%2fmid2020/ukpopestimatesmid2020on2021geography.xls"
+
+temp <- tempfile()
+temp <- curl_download(url=url, destfile=temp, quiet=FALSE, mode="wb")
+
+ONSpop_m <- read_excel(temp, sheet="MYE2 - Males", range="E8:CQ12") %>% 
+  slice_tail() %>% 
+  gather(Age, pop_m, c(1:ncol(.)))
+
+ONSpop_f <- read_excel(temp, sheet="MYE2 - Females", range="E8:CQ12") %>% 
+  slice_tail() %>% 
+  gather(Age, pop_f, c(1:ncol(.)))
+
+ONSpop <- merge(ONSpop_m, ONSpop_f) %>% 
+  mutate(Age=as.numeric(substr(Age, 1, 2)),
+         Age=case_when(
+           Age<12 ~ "Under 12",
+           Age<16 ~ "12 to under 16",
+           Age<18 ~ "16 to under 18",
+           Age<20 ~ "18 to under 20",
+           Age<25 ~ "20 to under 25",
+           Age<30 ~ "25 to under 30",
+           Age<35 ~ "30 to under 35",
+           Age<40 ~ "35 to under 40",
+           Age<45 ~ "40 to under 45",
+           Age<50 ~ "45 to under 50",
+           Age<55 ~ "50 to under 55",
+           Age<60 ~ "55 to under 60",
+           Age<65 ~ "60 to under 65",
+           Age<70 ~ "65 to under 70",
+           Age<75 ~ "70 to under 75",
+           Age<80 ~ "75 to under 80",
+           TRUE ~ "Over 80")) %>% 
+  group_by(Age) %>% 
+  summarise(pop_m=sum(pop_m), pop_f=sum(pop_f)) %>% 
+  ungroup()
+
+ONSdata <- merge(data, ONSpop) %>% 
+  mutate(Male_Unvax=pop_m-Male_Vax1Only-Male_Vax2,
+         Female_Unvax=pop_f-Female_Vax1Only-Female_Vax2)
+
+ONSdata_long <- pivot_longer(ONSdata, cols=c(2:7), names_to=c("Sex", "Measure"), names_sep="_", 
+                          values_to="Value") %>% 
+  mutate(Value=if_else(Sex=="Male", -Value, Value),
+         Age=case_when(
+           Age=="Under 12" ~ "<12",
+           Age=="12 to under 16" ~ "12-15",
+           Age=="16 to under 18" ~ "16-17",
+           Age=="18 to under 20" ~ "18-19",
+           Age=="20 to under 25" ~ "20-24",
+           Age=="25 to under 30" ~ "25-29",
+           Age=="30 to under 35" ~ "30-34",
+           Age=="35 to under 40" ~ "35-39",
+           Age=="40 to under 45" ~ "40-44",
+           Age=="45 to under 50" ~ "45-49",
+           Age=="50 to under 55" ~ "50-54",
+           Age=="55 to under 60" ~ "55-59",
+           Age=="60 to under 65" ~ "60-64",
+           Age=="65 to under 70" ~ "65-69",
+           Age=="70 to under 75" ~ "70-74",
+           Age=="75 to under 80" ~ "75-79",
+           TRUE ~ "80+"),
+         Age=factor(Age, levels=c("<12", "12-15", "16-17", "18-19", "20-24", "25-29", "30-34", "35-39", 
+                                  "40-44", "45-49", "50-54", "55-59", "60-64", "65-69", "70-74", "75-79", 
+                                  "80+")),
+         SexMeasure=paste0(Sex, Measure))
+
+agg_tiff("Outputs/COVIDVaxxAgexSexONS.tiff", units="in", width=8, height=7, res=800)
+ggplot(ONSdata_long, aes(x=Value, y=Age, fill=SexMeasure))+
+  geom_col(position="stack", show.legend=FALSE)+
+  scale_x_continuous(breaks=c(-5000000, -2500000, 0, 2500000, 5000000),
+                     labels=c("5m", "2.5m", "0", "2.5m", "5m"),
+                     limits=c(-5000000, 5000000), name="")+
+  scale_y_discrete(name="Age group")+
+  scale_fill_manual(values=c("Grey80", "#7dffdd", "#00cc99", "Grey80", "#be7dff", "#6600cc"))+
+  theme_classic()+
+  theme(axis.line.y=element_blank(), text=element_text(family="Lato"), 
+        plot.title=element_text(face="bold", size=rel(1.6)), plot.subtitle=element_markdown())+
+  labs(title="Vaccine delivery in England by age and sex",
+       subtitle="The number of people who are <span style='color:Grey60;'>unvaccinated</span>, men with <span style='color:#be7dff;'>one</span> or <span style='color:#6600cc;'>two</span> doses and women with <span style='color:#7dffdd;'>one</span> or <span style='color:#00cc99;'>two</span> doses",
+       caption="Data from PHE, population figures from ONS\nPlot by @VictimOfMaths")+
+  annotate("text", x=-4000000, y=9, label="Men", colour="#6600cc", size=rel(6), 
+           fontface="bold", family="Lato")+
+  annotate("text", x=4000000, y=9, label="Women", colour="#00cc99", size=rel(6), 
+           fontface="bold", family="Lato")
+dev.off()  
+
+agg_tiff("Outputs/COVIDVaxxAgexSexONSv2.tiff", units="in", width=8, height=7, res=800)
+ggplot(ONSdata_long %>% 
+         mutate(SexMeasure=factor(SexMeasure, levels=c("FemaleVax2", "FemaleVax1Only", "FemaleUnvax",
+                                                       "MaleVax2", "MaleVax1Only", "MaleUnvax"))), 
+       aes(x=Value, y=Age, fill=SexMeasure))+
+  geom_col(position="stack", show.legend=FALSE)+
+  geom_segment(aes(x=0, xend=0, y=0.5, yend=17.5))+
+  scale_x_continuous(breaks=c(-5000000, -2500000, 0, 2500000, 5000000),
+                     labels=c("5m", "2.5m", "0", "2.5m", "5m"),
+                     limits=c(-5000000, 5000000), name="")+
+  scale_y_discrete(name="Age group")+
+  scale_fill_manual(values=c("#00cc99", "#7dffdd", "Grey80",  "#6600cc", "#be7dff","Grey80"))+
+  theme_classic()+
+  theme(axis.line.y=element_blank(), text=element_text(family="Lato"), 
+        plot.title=element_text(face="bold", size=rel(1.6)), plot.subtitle=element_markdown())+
+  labs(title="Vaccine delivery in England by age and sex",
+       subtitle="The number of people who are <span style='color:Grey60;'>unvaccinated</span>, men with <span style='color:#be7dff;'>one</span> or <span style='color:#6600cc;'>two</span> doses and women with <span style='color:#7dffdd;'>one</span> or <span style='color:#00cc99;'>two</span> doses",
+       caption="Data from PHE, population figures from ONS\nPlot by @VictimOfMaths")+
+  annotate("text", x=-4000000, y=9, label="Men", colour="#6600cc", size=rel(6), 
+           fontface="bold", family="Lato")+
+  annotate("text", x=4000000, y=9, label="Women", colour="#00cc99", size=rel(6), 
+           fontface="bold", family="Lato")
+dev.off()  
+
+#####################################
 
 #Produce version with compressed age bands
 #V1 10 year bands for >20
