9de226bc4823b8f811fe9dff8a731a2dbbd8e4a9
Colin Angus <c.r.angus@sheffield.ac.uk>
2021-12-16 19:20:51 +0000

    Update COVIDAdmissionsWithFor.R

--- Heatmaps/COVIDAdmissionsWithFor.R
+++ Heatmaps/COVIDAdmissionsWithFor.R
@@ -6,6 +6,8 @@ library(readxl)
 library(paletteer)
 library(extrafont)
 library(ragg)
+library(lubridate)
+library(scales)
 
 theme_custom <- function() {
   theme_classic() %+replace%
@@ -67,6 +69,35 @@ ggplot(data %>% filter(Region=="London" & type!="with"),
   theme(plot.subtitle=element_markdown())+
   labs(title="London's rising number of COVID patients are both 'with' and 'for' COVID",
        subtitle="Patients in London hospitals <span style='color:#40A0D8;'>due to COVID</span> and admissions <span style='color:#F89088;'>where COVID is not the primary cause of the admission</span>",
+       caption="Data from NHS England | Plot by @VictimOfMaths")
+dev.off()
+
+agg_tiff("Outputs/COVIDAdmissionsCauseLondonStacked.tiff", units="in", width=9, height=7, res=500)
+ggplot(data %>% filter(Region=="London" & type!="with"), 
+       aes(x=date, y=count, fill=type))+
+  geom_area(position="stack", show.legend=FALSE)+
+  scale_x_date(name="")+
+  scale_y_continuous(name="Number of patients", limits=c(0,NA))+
+  scale_fill_paletteer_d("palettetown::porygon")+
+  theme_custom()+
+  theme(plot.subtitle=element_markdown())+
+  labs(title="London's rising number of COVID patients are both 'with' and 'for' COVID",
+       subtitle="Patients in London hospitals <span style='color:#40A0D8;'>due to COVID</span> and admissions <span style='color:#F89088;'>where COVID is not the primary cause of the admission</span>",
+       caption="Data from NHS England | Plot by @VictimOfMaths")
+dev.off()
+
+agg_tiff("Outputs/COVIDAdmissionsCauseLondonProp.tiff", units="in", width=9, height=7, res=500)
+data %>% filter(type!="incidental" & Region=="London") %>% 
+  spread(type, count) %>% 
+  mutate(prop=from/with) %>% 
+  ggplot(aes(x=date, y=prop))+
+  geom_line(colour="RoyalBlue")+
+  scale_x_date(name="")+
+  scale_y_continuous(name="Proportion of COVID patients for whom\nCOVID is the main cause of hospitalisation",
+                     limits=c(0,NA), labels=label_percent(accuracy=1))+
+  theme_custom()+
+  labs(title="COVID remains the main reason people with COVID end up in hospital in London",
+       subtitle="Proportion of total COVID-positive patients assessed to be in hospital 'for' rather than 'with' COVID in London NHS trusts",
        caption="Data from NHS England | Plot by @VictimOfMaths")
 dev.off()
 
