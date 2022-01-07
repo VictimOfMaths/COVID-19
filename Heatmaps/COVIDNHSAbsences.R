218a6dfb87482f38c8be61022d5c0a511b1867ac
Colin Angus <c.r.angus@sheffield.ac.uk>
2022-01-07 12:07:34 +0000

    Update COVIDNHSAbsences.R

--- Heatmaps/COVIDNHSAbsences.R
+++ Heatmaps/COVIDNHSAbsences.R
@@ -9,6 +9,7 @@ library(ragg)
 library(lubridate)
 library(scales)
 library(ggtext)
+library(geofacet)
 
 theme_custom <- function() {
   theme_classic() %+replace%
@@ -26,13 +27,14 @@ temp <- curl_download(url=source, destfile=temp, quiet=FALSE, mode="wb")
 
 totalraw <- read_excel(temp, sheet="Total Absences", range="C16:AM163", col_names=FALSE) 
   
+COVIDraw <- read_excel(temp, sheet="COVID Absences", range="C16:AM163", col_names=FALSE) 
+
+#Pull out national figures
 nattotals <- totalraw %>% 
   slice(1) %>% 
   gather(Date, Total, c(3:ncol(.))) %>% 
   select(-c(1,2)) %>% 
   mutate(Date=as.Date("2021-11-29")+days(as.numeric(substr(Date, 4, 6))-3))
-
-COVIDraw <- read_excel(temp, sheet="COVID Absences", range="C16:AM163", col_names=FALSE) 
 
 natCOVID <- COVIDraw %>% 
   slice(1) %>% 
@@ -58,5 +60,43 @@ ggplot(natdata %>% filter(Cause!="Total"), aes(x=Date, y=Count, fill=Cause))+
 
 dev.off()
 
-  
+#Regional version
+regtotals <- totalraw[c(3:9),] %>% 
+  gather(Date, Total, c(3:ncol(.))) %>% 
+  select(-c(1)) %>% 
+  rename(Region=`...2`) %>% 
+  mutate(Date=as.Date("2021-11-29")+days(as.numeric(substr(Date, 4, 6))-3))
+
+regCOVID <- COVIDraw[c(3:9),] %>% 
+  gather(Date, COVID, c(3:ncol(.))) %>% 
+  select(-c(1)) %>% 
+  rename(Region=`...2`) %>%   
+  mutate(Date=as.Date("2021-11-29")+days(as.numeric(substr(Date, 4, 6))-3))
+
+regdata <- merge(regtotals, regCOVID) %>% 
+  mutate(Other=Total-COVID) %>% 
+  gather(Cause, Count, c(3:5))
+
+#Set up geofacet grid of NHS regions
+mygrid <- data.frame(name=c("North West", "North East and Yorkshire", 
+                            "Midlands","East of England",
+                            "South West", "London", "South East"),
+                     row=c(1,1,2,2,3,3,3), col=c(2,3,2,3,1,2,3),
+                     code=c(1:7))
+
+agg_tiff("Outputs/COVIDNHSAbsencesxReg.tiff", units="in", width=8, height=6, res=500)
+ggplot(regdata %>% filter(Cause!="Total"), aes(x=Date, y=Count, fill=Cause))+
+  geom_area(show.legend=FALSE)+
+  scale_x_date(name="")+
+  scale_y_continuous(name="Total staff absent")+
+  scale_fill_paletteer_d("lisa::Jean_MichelBasquiat_1")+
+  facet_geo(~Region, grid=mygrid)+
+  theme_custom()+
+  theme(plot.subtitle=element_markdown())+
+  labs(title="NHS staff absences are most acute in the Midlands and North of England",
+       subtitle="Staff ill or isolating <span style='color:#C11432FF;'>due to COVID</span> or <span style='color:#009ADAFF ;'>absent for other reasons</span> in English acute NHS trusts",
+       caption="Data from NHS England | Plot by @VictimOfMaths")
+
+dev.off()
+
   
