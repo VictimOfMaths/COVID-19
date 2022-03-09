759071b881979adb8b782937722df7e75e2ff5dd
Colin Angus <c.r.angus@sheffield.ac.uk>
2022-03-09 10:27:19 +0000

    Update COVIDMetricsNormalised.R

--- Heatmaps/COVIDMetricsNormalised.R
+++ Heatmaps/COVIDMetricsNormalised.R
@@ -219,3 +219,51 @@ ggplot(regdata %>% filter(Region=="London" & date>as.Date("2020-04-01")),
        caption="Plot inspired by @PaulMainwood | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths\n\n*Admissions data is based on the London NHS region, which does not exactly match the government region that cases and deaths data is based on")
 
 dev.off()
+
+####################
+#Version for Omicron only
+data2 <- read.csv(temp) %>% 
+  select(-c(1:3)) %>% 
+  set_names(c("date", "Admissions", "Cases", "Deaths")) %>% 
+  gather(metric, count, c(2:4)) %>% 
+  mutate(date=as.Date(date),
+         metric=factor(metric, levels=c("Cases", "Admissions", "Deaths"))) %>% 
+  group_by(metric) %>% 
+  #Calculate rolling means
+  mutate(count_roll=roll_mean(count, 7, align="center", fill=NA)) %>% 
+  #Normalise against the peak.
+  #Step 1 - grab the peak value for each metric
+  #Option 1, just using the max value across the entire time series
+  #mutate(max=max(count, na.rm=TRUE)) %>% 
+  #Option 2, normalising against the max value within a chosen date range
+  mutate(max=max(count_roll[date>as.Date("2021-10-01") & date<as.Date("2022-02-01")], 
+                 na.rm=TRUE)) %>% 
+  ungroup() %>% 
+  #Step 2 - calculate each date's value as a proportion of the peak
+  mutate(prop=count_roll/max)
+
+#Simple replication
+ggplot(data2, aes(x=date, y=prop, colour=metric))+
+  geom_line()
+
+#Prettier version (because why not)
+
+agg_tiff("Outputs/COVIDMetricsNormalisedOmi.tiff", units="in", width=9, height=6, res=500)
+ggplot(data2 %>% filter(date>as.Date("2021-12-01")), 
+       aes(x=date, y=prop, colour=metric))+
+  geom_line(show.legend=FALSE)+
+  scale_x_date(name="", date_labels="%B %y")+
+  scale_y_continuous(labels=label_percent(accuracy=1), 
+                     name="Level as a proportion of Omicron peak",
+                     limits=c(0,1))+
+  scale_colour_paletteer_d("wesanderson::Darjeeling1")+
+  theme_classic()+
+  theme(text=element_text(family="Lato"), plot.title.position="plot",
+        plot.title=element_text(face="bold", size=rel(1.5)),
+        plot.subtitle=element_markdown(),
+        panel.grid.major.y=element_line(colour="Grey90"))+
+  labs(title="Both admissions and cases have started rising in recent days",
+       subtitle="Rolling 7-day average of new COVID <span style='color:#FF0000;'>cases</span>, <span style='color:#00A08A;'>admissions</span> and <span style='color:#F2AD00;'>deaths</span> in England as a proportion of their peak value in January 2022",
+       caption="Plot inspired by @PaulMainwood | Data from coronavirus.data.gov.uk | Plot by @VictimOfMaths")
+
+dev.off()
